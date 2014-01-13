8d52e8d75d11eaded8284799362cbd45f4c82167
Phil Freeman <paf31@cantab.net>
2014-01-12 18:50:51 -0800

    Improved jQuery library and example

--- libraries/jquery/tests/jquery.purs
+++ libraries/jquery/tests/jquery.purs
@@ -1,11 +1,28 @@
 module Main where
 
+import Prelude
 import Eff
+import Trace
 import JQuery
 
 main = eff do
+  -- Get the document body
   b <- body
+
+  -- Create a text box
   div <- create "<div>"
-  { color: "red" } `css` div
-  appendText "Hello World" div
+  input <- create "<input>"
+  "Your Name: " `appendText` div
+  input `append` div
   div `append` b
+
+  -- Create a paragraph to display a greeting
+  greeting <- create "<p>"
+  { color: "red" } `css` greeting
+  greeting `append` b
+
+  -- Listen for change events on the text box
+  flip (on "change") input $ eff do
+    name <- getValue input
+    trace $ "Name changed to " ++ name
+    setText ("Hello, " ++ name) greeting
