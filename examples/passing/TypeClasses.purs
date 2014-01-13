199d052d69bd79009d41b9703a2a8f0176ea5d5b
Phil Freeman <paf31@cantab.net>
2014-01-12 20:46:27 -0800

    More work on desugaring

--- examples/passing/TypeClasses.purs
+++ examples/passing/TypeClasses.purs
@@ -3,5 +3,5 @@ module TypeClasses where
 class Show a where
   show :: a -> String
 
-instance Show String where
+instance TypeClasses.Show String where
   show s = s
--- src/Language/PureScript/Sugar/TypeClasses.hs
+++ src/Language/PureScript/Sugar/TypeClasses.hs
@@ -22,26 +22,37 @@ import Language.PureScript.Types
 import Language.PureScript.Values
 import Language.PureScript.CodeGen.JS.AST
 
+import qualified Data.Map as M
+
+import Control.Applicative
+import Control.Monad.State
+import Data.Maybe (fromMaybe)
+
+type MemberMap = M.Map (ModuleName, ProperName) [(String, Type)]
+
 desugarTypeClasses :: [Module] -> [Module]
-desugarTypeClasses = map desugarModule
+desugarTypeClasses = flip evalState M.empty . mapM desugarModule
 
-desugarModule :: Module -> Module
-desugarModule (Module name decls) = Module name $ concatMap desugarDecl decls
+desugarModule :: Module -> State MemberMap Module
+desugarModule (Module name decls) = Module name <$> concat <$> mapM (desugarDecl (ModuleName name)) decls
 
-desugarDecl :: Declaration -> [Declaration]
-desugarDecl (TypeClassDeclaration name arg members) =
-  typeClassDictionaryDeclaration name arg members : map (typeClassMemberToDictionaryAccessor name arg) members
-desugarDecl (TypeInstanceDeclaration deps name ty members) =
-  typeInstanceDictionaryDeclaration deps name ty members : map (typeInstanceDictionaryEntryDeclaration deps name ty) members
-desugarDecl other = [other]
+desugarDecl :: ModuleName -> Declaration -> State MemberMap [Declaration]
+desugarDecl mn (TypeClassDeclaration name arg members) = do
+  let tys = map memberToNameAndType members
+  modify (M.insert (mn, name) tys)
+  return $ typeClassDictionaryDeclaration name arg members : map (typeClassMemberToDictionaryAccessor name arg) members
+desugarDecl mn (TypeInstanceDeclaration deps name ty members) = do
+  entries <- mapM (typeInstanceDictionaryEntryDeclaration mn deps name ty) members
+  return $ typeInstanceDictionaryDeclaration deps name ty members : entries
+desugarDecl _ other = return [other]
+
+memberToNameAndType :: Declaration -> (String, Type)
+memberToNameAndType (TypeDeclaration ident ty) = (identToString ident, ty)
+memberToNameAndType _ = error "Invalid declaration in type class definition"
 
 typeClassDictionaryDeclaration :: ProperName -> String -> [Declaration] -> Declaration
 typeClassDictionaryDeclaration name arg members =
   TypeSynonymDeclaration name [arg] (Object $ rowFromList (map memberToNameAndType members, REmpty))
-  where
-  memberToNameAndType :: Declaration -> (String, Type)
-  memberToNameAndType (TypeDeclaration ident ty) = (identToString ident, ty)
-  memberToNameAndType _ = error "Invalid declaration in type class definition"
 
 typeClassMemberToDictionaryAccessor :: ProperName -> String -> Declaration -> Declaration
 typeClassMemberToDictionaryAccessor name arg (TypeDeclaration ident ty) =
@@ -61,18 +72,35 @@ typeInstanceDictionaryDeclaration deps name ty decls =
     (identToString ident, JSVar $ mkDictionaryEntryName name ty ident)
   memberToNameAndValue _ = error "Invalid declaration in type instance definition"
 
-typeInstanceDictionaryEntryDeclaration :: [(Qualified ProperName, Type)] -> Qualified ProperName -> Type -> Declaration -> Declaration
-typeInstanceDictionaryEntryDeclaration deps name ty (ValueDeclaration ident binders guard val) =
-  ValueDeclaration (mkDictionaryEntryName name ty ident) binders guard
-    (TypedValue val (ConstrainedType deps undefined))
-typeInstanceDictionaryEntryDeclaration _ _ _ _ = error "Invalid declaration in type instance definition"
+typeInstanceDictionaryEntryDeclaration :: ModuleName -> [(Qualified ProperName, Type)] -> Qualified ProperName -> Type -> Declaration -> State MemberMap Declaration
+typeInstanceDictionaryEntryDeclaration mn deps name ty (ValueDeclaration ident binders grd val) = do
+  m <- get
+  let valTy = fromMaybe (error "Type class instance dictionary member is missing in typeInstanceDictionaryEntryDeclaration") $ do
+                members <- M.lookup (qualify mn name) m
+                lookup (identToString ident) members
+  return $ ValueDeclaration (mkDictionaryEntryName name ty ident) binders grd
+    (TypedValue val (ConstrainedType deps valTy))
+typeInstanceDictionaryEntryDeclaration _ _ _ _ _ = error "Invalid declaration in type instance definition"
 
 identToString :: Ident -> String
 identToString (Ident s) = s
-identToString (Op _) = error "Unsupported type class instance name"
+identToString (Op _) = error "TODO: Unsupported type class instance name"
+
+qualifiedToString :: Qualified ProperName -> String
+qualifiedToString (Qualified Nothing _) = error "TODO: handle unqualified instances"
+qualifiedToString (Qualified (Just (ModuleName mn)) pn) = runProperName mn ++ "_" ++ runProperName pn
 
 mkDictionaryValueName :: Qualified ProperName -> Type -> Ident
-mkDictionaryValueName _ _ = Ident "__dict"
+mkDictionaryValueName cl ty = Ident $ "__" ++ qualifiedToString cl ++ "_" ++ typeToString ty
+  where
+  typeToString :: Type -> String
+  typeToString String = "string"
+  typeToString Number = "number"
+  typeToString Boolean = "boolean"
+  typeToString (Array (TypeVar _)) = "array"
+  typeToString (TypeConstructor ty') = qualifiedToString ty'
+  typeToString (TypeApp ty' (TypeVar _)) = typeToString ty'
+  typeToString _ = error "TODO: Type class instance must be of the form T a1 ... an"
 
 mkDictionaryEntryName :: Qualified ProperName -> Type -> Ident -> Ident
 mkDictionaryEntryName name ty ident = let Ident dictName = mkDictionaryValueName name ty
