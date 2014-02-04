15a0842aa851fc06ef4d0222219c0f33e34f7958
Phil Freeman <paf31@cantab.net>
2014-02-04 11:29:15 -0800

    Improve speed by using a HashMap to store results

--- src/Control/Monad/Unify.hs
+++ src/Control/Monad/Unify.hs
@@ -16,10 +16,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE UndecidableInstances #-}
@@ -35,6 +32,8 @@ import Control.Applicative
 import Control.Monad.State
 import Control.Monad.Error
 
+import Data.HashMap.Strict as M
+
 -- |
 -- Untyped unification variables
 --
@@ -46,37 +45,46 @@ newtype Unknown = Unknown {
   } deriving (Show, Eq, Ord, Data, Typeable)
 
 -- |
--- The type of typed unification variables
+-- A type which can contain unification variables
 --
-newtype TypedUnknown t = TypedUnknown {
-  -- |
-  -- The underlying untyped unification variable
-  --
-    runTypedUnknown :: Unknown
-  } deriving (Show, Eq, Ord, Data, Typeable)
+class (Typeable t, Data t) => Partial t where
+  unknown :: Unknown -> t
+  isUnknown :: t -> Maybe Unknown
 
 -- |
 -- Identifies types which support unification
 --
-class (Typeable t, Data t, Show t) => Unifiable m t | t -> m where
-  unknown :: TypedUnknown t -> t
-  isUnknown :: t -> Maybe (TypedUnknown t)
-  (?=) :: t -> t -> UnifyT m ()
+class (Partial t) => Unifiable m t | t -> m where
+  (=?=) :: t -> t -> UnifyT t m ()
 
 -- |
--- A substitution maintains a mapping from unification variables to their values, ensuring that
--- the type of a unification variable matches the type of its value.
+-- A substitution maintains a mapping from unification variables to their values
 --
-newtype Substitution = Substitution { runSubstitution :: forall d. (Data d) => d -> d }
+data Substitution t = Substitution { runSubstitution :: M.HashMap Int t }
 
-instance Monoid Substitution where
-  mempty = Substitution id
-  s1 `mappend` s2 = Substitution $ runSubstitution s1 . runSubstitution s2
+instance (Partial t) => Monoid (Substitution t) where
+  mempty = Substitution M.empty
+  s1 `mappend` s2 = Substitution $
+                      M.map (s2 $?) (runSubstitution s1) `M.union`
+                      M.map (s1 $?) (runSubstitution s2)
 
 -- |
--- State required for type checking:
+-- Apply a substitution to a value
 --
-data UnifyState = UnifyState {
+($?) :: (Partial t) => Substitution t -> t -> t
+($?) sub = everywhere (mkT go)
+  where
+  go t =
+    case isUnknown t of
+      Nothing -> t
+      Just (Unknown u) -> case M.lookup u (runSubstitution sub) of
+                  Nothing -> t
+                  Just t' -> t'
+
+-- |
+-- State required for type checking
+--
+data UnifyState t = UnifyState {
   -- |
   -- The next fresh unification variable
   --
@@ -84,22 +92,22 @@ data UnifyState = UnifyState {
   -- |
   -- The current substitution
   --
-  , unifyCurrentSubstitution :: Substitution
+  , unifyCurrentSubstitution :: Substitution t
   }
 
 -- |
 -- An empty @UnifyState@
 --
-defaultUnifyState :: UnifyState
+defaultUnifyState :: (Partial t) => UnifyState t
 defaultUnifyState = UnifyState 0 mempty
 
 -- |
 -- The type checking monad, which provides the state of the type checker, and error reporting capabilities
 --
-newtype UnifyT m a = UnifyT { unUnify :: StateT UnifyState (ErrorT String m) a }
+newtype UnifyT t m a = UnifyT { unUnify :: StateT (UnifyState t) (ErrorT String m) a }
   deriving (Functor, Monad, Applicative, MonadPlus, MonadError String)
 
-instance (MonadState s m) => MonadState s (UnifyT m) where
+instance (MonadState s m) => MonadState s (UnifyT t m) where
   get = UnifyT . lift $ get
   put = UnifyT . lift . put
 
@@ -114,39 +122,35 @@ unknowns = everything (++) (mkQ [] collect)
 -- |
 -- Run a computation in the Unify monad, failing with an error, or succeeding with a return value and the new next unification variable
 --
-runUnify :: UnifyState -> UnifyT m a -> m (Either String (a, UnifyState))
+runUnify :: UnifyState t -> UnifyT t m a -> m (Either String (a, UnifyState t))
 runUnify s = runErrorT . flip runStateT s . unUnify
 
 -- |
 -- Substitute a single unification variable
 --
-substituteOne :: (Unifiable m t) => TypedUnknown t -> t -> Substitution
-substituteOne u t = Substitution $ everywhere (mkT go)
-  where
-  go t' = case isUnknown t' of
-           Just u1 | u1 == u -> t
-           _ -> t'
+substituteOne :: (Partial t) => Unknown -> t -> Substitution t
+substituteOne (Unknown u) t = Substitution $ M.singleton u t
 
 -- |
 -- Replace a unification variable with the specified value in the current substitution
 --
-replace :: (Monad m, Unifiable m t) => TypedUnknown t -> t -> UnifyT m ()
-replace u t' = do
+(=:=) :: (Monad m, Unifiable m t) => Unknown -> t -> UnifyT t m ()
+(=:=) u t' = do
   st <- UnifyT get
   let sub = unifyCurrentSubstitution st
-  let t = runSubstitution sub t'
+  let t = sub $? t'
   occursCheck u t
-  let current = runSubstitution sub $ unknown u
+  let current = sub $? unknown u
   case isUnknown current of
     Just u1 | u1 == u -> return ()
-    _ -> current ?= t
+    _ -> current =?= t
   UnifyT $ modify $ \s -> s { unifyCurrentSubstitution = substituteOne u t <> unifyCurrentSubstitution s }
 
 -- |
 -- Perform the occurs check, to make sure a unification variable does not occur inside a value
 --
-occursCheck :: (Monad m, Unifiable m t) => TypedUnknown s -> t -> UnifyT m ()
-occursCheck (TypedUnknown u) t =
+occursCheck :: (Monad m, Partial t) => Unknown -> t -> UnifyT t m ()
+occursCheck u t =
   case isUnknown t of
     Nothing -> when (u `elem` unknowns t) $ UnifyT . lift $ throwError "Occurs check fails"
     _ -> return ()
@@ -154,7 +158,7 @@ occursCheck (TypedUnknown u) t =
 -- |
 -- Generate a fresh untyped unification variable
 --
-fresh' :: (Monad m) => UnifyT m Unknown
+fresh' :: (Monad m) => UnifyT t m Unknown
 fresh' = do
   st <- UnifyT get
   UnifyT $ modify $ \s -> s { unifyNextVar = succ (unifyNextVar s) }
@@ -163,8 +167,8 @@ fresh' = do
 -- |
 -- Generate a fresh unification variable at a specific type
 --
-fresh :: (Monad m, Unifiable m t) => UnifyT m t
+fresh :: (Monad m, Partial t) => UnifyT t m t
 fresh = do
   u <- fresh'
-  return $ unknown $ TypedUnknown u
+  return $ unknown u
 
