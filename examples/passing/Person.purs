64289a1e90e74198a313f90bac606c2f173873b8
Phil Freeman <paf31@cantab.net>
2014-02-06 13:16:35 -0800

    Fix broken tests

--- examples/passing/Person.purs
+++ examples/passing/Person.purs
@@ -2,13 +2,13 @@ module Person where
 
   import Prelude ((++))
 
-  data Person = Person { name :: String, age :: Number }
+  data Person = MkPerson { name :: String, age :: Number }
 
   foreign import itoa :: Number -> String
 	  
   showPerson :: Person -> String
   showPerson = \p -> case p of
-    Person o -> o.name ++ ", aged " ++ itoa(o.age)
+    MkPerson o -> o.name ++ ", aged " ++ itoa(o.age)
     
 module Main where
 
