473f116caaa4a67f147bc7c40e9ac881cf827eed
Phil Freeman <paf31@cantab.net>
2014-02-10 12:27:33 -0800

    Minimal failure case for runtime scope issue involving type classes

---
+++ examples/shouldpass/RuntimeScopeIssue.purs
@@ -0,0 +1,25 @@
+module RuntimeScopeIssue where
+
+import Prelude
+
+class A a where
+  a :: a -> Boolean
+
+class B a where
+  b :: a -> Boolean
+
+instance A Number where
+  a 0 = true
+  a n = b (n - 1)
+
+instance B Number where
+  b 0 = false
+  b n = a (n - 1)
+
+module Main where
+
+import RuntimeScopeIssue
+
+import Prelude
+
+main = Trace.print $ a 10
