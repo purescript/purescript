3318082efddfb2a9e7bf5f439869d33fb3ac9fc8
Phil Freeman <paf31@cantab.net>
2014-02-12 16:51:45 -0800

    Expand STArray example

--- examples/passing/STArray.purs
+++ examples/passing/STArray.purs
@@ -11,4 +11,13 @@ test = runSTArray (do
   pokeSTArray a 1 2
   return a)
 
-main = Trace.print $ runPure test
+fromTo' lo hi _ arr | lo > hi = return arr
+fromTo' lo hi i arr = do
+  pokeSTArray arr i lo
+  fromTo' (lo + 1) hi (i + 1) arr
+
+fromTo lo hi = runSTArray (do
+  arr <- newSTArray (hi - lo + 1) 0
+  fromTo' lo hi 0 arr)
+
+main = Trace.print $ runPure (fromTo 10 20)
