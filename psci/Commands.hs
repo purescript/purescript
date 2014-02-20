9b7790fd69ecbff493f5930bd60a5aea849ef64b
joneshf <jones3.hardy@gmail.com>
2014-02-19 23:42:03 -0800

    Some documentation.

--- psci/Commands.hs
+++ psci/Commands.hs
@@ -1,18 +1,63 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  Main
+-- Copyright   :  (c) Phil Freeman 2014
+-- License     :  MIT
+--
+-- Maintainer  :  Phil Freeman <paf31@cantab.net>
+-- Stability   :  experimental
+-- Portability :
+--
+-- |
+-- Commands for PSCI.
+--
+-----------------------------------------------------------------------------
+
 module Commands where
 
 import Control.Applicative
 import System.Console.Haskeline
 
+-- |
+-- Valid Meta-commands for PSCI
+--
 data Command
+  -- |
+  -- And empty line
+  --
   = Empty
+  -- |
+  -- A purescript expression
+  --
   | Expression [String]
+  -- |
+  -- Show the help command
+  --
   | Help
+  -- |
+  -- Import a module from a loaded file
+  --
   | Import String
+  -- |
+  -- Load a file for use with importing
+  --
   | LoadFile FilePath
+  -- |
+  -- Exit PSCI
+  --
   | Quit
+  -- |
+  -- Reload the current modules
+  --
   | Reload
+  -- |
+  -- An attempt at a meta command that wasn't recognized.
+  --
   | Unknown deriving (Show, Eq)
 
+-- |
+-- Parses the input and returns either a Metacommand or an expression.
+--
 getCommand :: InputT IO Command
 getCommand = do
   firstLine <- getInputLine "> "
@@ -32,6 +77,9 @@ getCommand = do
       Nothing -> return $ reverse ls
       Just l' -> go (l' : ls)
 
+-- |
+-- The help menu.
+--
 help :: [[String]]
 help =
   [ [":?         ", "Show this help menu"]
