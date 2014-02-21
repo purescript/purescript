e3e80c135a8d9cc0f82daa6e060e03eb9bf835c9
joneshf <jones3.hardy@gmail.com>
2014-02-20 17:54:55 -0800

    Process os stuff better.

--- Setup.hs
+++ Setup.hs
@@ -1,6 +1,16 @@
 {-# LANGUAGE CPP #-}
 
+#ifndef POSIX_LIKE
+#define POSIX_LIKE !defined(_WIN32_HOST_OS) && \
+                  (defined(unix_HOST_OS) || defined(__unix___HOST_OS) || \
+                   defined(__unix_HOST_OS) || defined(linux_HOST_OS) || \
+                   defined(__linux___HOST_OS) || defined(__linux_HOST_OS) || \
+                   (defined(__APPLE___HOST_OS) && defined(__MACH___HOST_OS)))
+#endif
+
 module Main where
+
+import Control.Monad
 
 import Distribution.PackageDescription
 import Distribution.Simple
@@ -9,7 +19,7 @@ import Distribution.Simple.Setup
 
 import System.Directory
 import System.Environment.XDG.BaseDir
-#if !defined(mingw32_HOST_OS) || !defined(__MINGW32__)
+#if POSIX_LIKE
 import System.Posix.Files
 #endif
 
@@ -19,7 +29,11 @@ main = defaultMainWithHooks $ simpleUserHooks {postInst = setupXDG}
 setupXDG :: Args -> InstallFlags -> PackageDescription -> LocalBuildInfo -> IO ()
 setupXDG _ _ _ _ = do
     configDir <- getUserConfigDir "purescript"
-    createDirectoryIfMissing True configDir
-#if !defined(mingw32_HOST_OS) || !defined(__MINGW32__)
-    setFileMode configDir ownerModes
+    configExists <- doesDirectoryExist configDir
+    putStrLn "\n\n\noh it worked!\n\n\n"
+    unless configExists $ do
+        createDirectoryIfMissing True configDir
+#if POSIX_LIKE
+        setFileMode configDir ownerModes
 #endif
+#undef POSIX_LIKE
