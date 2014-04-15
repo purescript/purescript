6dc044178fa99e0cdef72017b6a3f00390bc8590
Phil Freeman <paf31@cantab.net>
2014-04-15 10:49:51 -0700

    Include images for online documentation

--- prelude/updateDocs.sh
+++ prelude/updateDocs.sh
@@ -1,9 +1,11 @@
 ../dist/build/docgen/docgen prelude.purs > README.md
 ../dist/build/hierarchy/hierarchy prelude.purs -o prelude
 mkdir -p images
+mkdir -p ../docs/source/images
 for f in prelude/*; do
     BASE=$(basename "$f")
     dot -Tpng -o images/"$BASE".png "$f"
+    cp images/"$BASE".png ../docs/source/images/"$BASE".png
 done
 rm -r prelude
 pandoc -o ../docs/source/prelude.rst README.md
