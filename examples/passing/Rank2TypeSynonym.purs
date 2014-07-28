9e188fc53c9452e6a808284c9450d82537391a53
Phil Freeman <paf31@cantab.net>
2014-07-27 18:56:45 -0700

    Satisfy type class dictionaries when LHS of a function application is constrained

---
+++ examples/passing/Rank2TypeSynonym.purs
@@ -0,0 +1,15 @@
+module Main where
+
+import Control.Monad.Eff
+
+type Foo a = forall f. (Monad f) => f a
+ 
+foo :: forall a. a -> Foo a
+foo x = pure x
+ 
+bar :: Foo Number
+bar = foo 3
+
+main = do
+  x <- bar
+  Debug.Trace.print x
