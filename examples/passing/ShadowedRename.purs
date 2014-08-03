58091f4c626dc159c76f9abe2695ae275bde5434
Gary Burgess <gary.burgess@gmail.com>
2014-08-03 21:30:22 +0100

    Test for accidental renamer ident shadowing

---
+++ examples/passing/ShadowedRename.purs
@@ -0,0 +1,19 @@
+module Main where
+
+import Control.Monad.Eff
+import Debug.Trace
+
+foreign import f
+  "function f(x) {\
+  \  return function () {\
+  \    if (x !== 2) throw new Error('x is not 2');\
+  \  }\
+  \}" :: forall e. Number -> Eff e Number
+
+foo foo = let foo_1 = \_ -> foo
+              foo_2 = foo_1 unit + 1
+          in foo_2
+
+main = do
+  f (foo 1)
+  trace "Done"
