69f61e90a98ab3bfd34c532dbecf0cb2dc9bc67b
Gary Burgess <gary.burgess@gmail.com>
2014-11-23 15:30:03 +0000

    Fix test
    Without these definitions the test fails when DCE is disabled

--- examples/passing/ForeignInstance.purs
+++ examples/passing/ForeignInstance.purs
@@ -9,6 +9,10 @@ foreign import instance fooNumber :: Foo Number
 
 foreign import instance fooString :: Foo String
 
+foreign import fooString "var fooString = {};" :: Unit
+foreign import fooNumber "var fooNumber = {};" :: Unit
+foreign import fooArray "var fooArray = {};" :: Unit
+
 test1 _ = foo [1, 2, 3]
 
 test2 _ = foo "Test"
