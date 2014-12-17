bc506cc7a539c0bc4a3f4c0038b084e8d971a6ad
Phil Freeman <paf31@cantab.net>
2014-12-16 21:35:38 -0800

    Reorganize docs

--- prelude/updateDocs.sh
+++ prelude/updateDocs.sh
@@ -1,4 +1,11 @@
-../dist/build/psc-docs/psc-docs --hierarchy-images modules/*.purs > README.md
+../dist/build/psc-docs/psc-docs --hierarchy-images \
+  modules/Prelude.purs \
+  modules/Data.Function.purs \
+  modules/Prelude.Unsafe.purs \
+  modules/Control.Monad.Eff.Unsafe.purs \
+  modules/Debug.Trace.purs \
+  modules/Control.Monad.ST.purs \
+  > README.md
 ../dist/build/hierarchy/hierarchy modules/Prelude.purs -o prelude
 
 mkdir -p images
