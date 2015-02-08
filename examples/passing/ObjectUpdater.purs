b3d3f79ddb779a80527397767113bdea685088d5
Gary Burgess <gary.burgess@gmail.com>
2015-02-08 01:13:03 +0000

    More failing tests

--- examples/passing/ObjectUpdater.purs
+++ examples/passing/ObjectUpdater.purs
@@ -3,21 +3,30 @@ module Main where
 import Control.Monad.Eff
 import Debug.Trace
 
-foreign import data Error :: !
-
-foreign import error
+foreign import eqeqeq
   """
-  function error() {
-    throw new Error("Object update failed");
-  }
-  """ :: forall e. Eff (error :: Error | e) Unit
+  function eqeqeq(x) {
+    return function (y) {
+      if (x == y) return x;
+      throw new Error("Unexpected result: " + x + " /== " + y);
+    };
+  };
+  """ :: forall a. a -> a -> a
+
+(===) = eqeqeq
+infixl 4 ===
 
 getValue :: forall e. Eff (| e) Boolean
 getValue = return true
 
 main = do
   let record = { value: false }
-  record2 <- record { value = _ } <$> getValue
-  if record2.value
-    then trace "Done"
-    else error
+  record' <- record { value = _ } <$> getValue
+  print $ record'.value === true
+
+  let point = { x: 1, y: 1 }
+      x = 10
+      point' = (point { x = _, y = x }) 100
+
+  print $ point'.x === 100
+  print $ point'.y === 10
--- examples/passing/ObjectWildcards.purs
+++ examples/passing/ObjectWildcards.purs
@@ -26,6 +26,6 @@ main = do
   print obj.value
   let x = 1
   point <- { x: _, y: x } <$> return 2
-  print $ point.x === 1
-  print $ point.y === 2
+  print $ point.x === 2
+  print $ point.y === 1
   trace (mkRecord 1 "Done!").bar
