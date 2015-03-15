8538fbaf504c12ef303e2b4e159a9d4ff70b4663
Nicolas Del Piano <ndel314@gmail.com>
2015-03-15 14:35:38 -0300

    src/Language/PureScript
    Created a new module for handling warnings.
    Adding warning capability to the typechecker monad (being discussed, not done yet).

---
+++ src/Language/PureScript/Warnings.hs
@@ -0,0 +1,70 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  Language.PureScript.Warnings
+-- Copyright   :  (c) 2013-14 Phil Freeman, (c) 2014 Gary Burgess, and other contributors
+-- License     :  MIT
+--
+-- Maintainer  :  Phil Freeman <paf31@cantab.net>
+-- Stability   :  experimental
+-- Portability :
+--
+-- |
+--
+-----------------------------------------------------------------------------
+
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module Language.PureScript.Warnings where
+
+import Data.Either (lefts, rights)
+import Data.List (intercalate)
+import Data.Monoid
+import Data.Foldable (fold, foldMap)
+
+import Control.Monad.Except
+import Control.Monad.Unify
+import Control.Applicative ((<$>))
+
+import Language.PureScript.AST
+import Language.PureScript.Pretty
+import Language.PureScript.Types
+import Language.PureScript.Names
+import Language.PureScript.Kinds
+import Language.PureScript.TypeClassDictionaries
+
+import qualified Text.PrettyPrint.Boxes as Box
+
+-- |
+-- A type of warning messages
+--
+data WarningMessage
+  = UnusedTypeVariable Type
+  | ShadowedTypeVariable Type
+  deriving (Show)
+
+-- |
+-- Get the warning code for a particular warning type
+--
+warningCode :: WarningMessage -> String
+warningCode (UnusedTypeVariable _)              = "UnusedTypeVariable"
+warningCode (ShadowedTypeVariable _)            = "ShadowedTypeVariable"
+
+-- |
+-- A stack trace for a warning
+--
+newtype MultipleWarnings = MultipleWarnings
+  { runMultipleWarnings :: [WarningMessage] } deriving (Show, Monoid)
+
+-- |
+-- Create a warning set from a single warning message
+--
+warningMessage :: WarningMessage -> MultipleWarnings
+warningMessage war = MultipleWarnings [war]
+
+-- |
+-- Lift a function on WarningMessage to a function on MultipleWarnings
+--
+onWarningMessages :: (WarningMessage -> WarningMessage) -> MultipleWarnings -> MultipleWarnings
+onWarningMessages f = MultipleWarnings . map f . runMultipleWarnings
