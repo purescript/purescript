354026f148b9c4e994da5fab47fa4d1bc4e36c49
Łukasz Hanuszczak <lukasz.hanuszczak@gmail.com>
2015-03-18 22:18:18 +0100

    Add some more sophisticated cases to row constructors test suite

--- examples/passing/RowConstructors.purs
+++ examples/passing/RowConstructors.purs
@@ -22,4 +22,19 @@ bar' = id' bar
 baz :: Baz
 baz = { x: 0, y: 0, z: 0, w: 0 }
 
+type Quux r = (q :: Number | r)
+type Norf r = (q' :: Number | Quux r)
+
+quux :: { f :: { | Foo } | Quux Bar }
+quux = { f: foo', x: 0, y: 0, z: 0, q: 0 }
+
+quux' :: { | Norf Bar }
+quux' = { x: 0, y: 0, z: 0, q: 0, q': 0 }
+
+wildcard :: { w :: Number | _ } -> Baz
+wildcard { w: w } = { x: w, y: w, z: w, w: w }
+
+wildcard' :: { | Quux _ } -> Number
+wildcard' { q: q } = q
+
 main = Debug.Trace.trace "Done"
