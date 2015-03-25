8d006d9f1fae163f41bf6f7af7728d3a72482772
Phil Freeman <paf31@cantab.net>
2015-03-25 10:29:25 -0700

    Fix #365

--- src/Language/PureScript/Sugar/BindingGroups.hs
+++ src/Language/PureScript/Sugar/BindingGroups.hs
@@ -157,7 +157,6 @@ getProperName _ = error "Expected DataDeclaration"
 --
 toBindingGroup :: (Functor m, MonadError MultipleErrors m) => ModuleName -> SCC Declaration -> m Declaration
 toBindingGroup _ (AcyclicSCC d) = return d
-toBindingGroup _ (CyclicSCC [d]) = return d
 toBindingGroup moduleName (CyclicSCC ds') =
   -- Once we have a mutually-recursive group of declarations, we need to sort
   -- them further by their immediate dependencies (those outside function
