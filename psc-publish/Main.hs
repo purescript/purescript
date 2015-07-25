20209ff21bd405edacd3a21fa19048a3f13c6719
Harry Garrood <harry@garrood.me>
2015-07-25 18:49:03 +0100

    Add --dry-run option to psc-publish

--- psc-publish/Main.hs
+++ psc-publish/Main.hs
@@ -1,19 +1,24 @@
 
 module Main where
 
-import Data.Version (showVersion)
+import Data.Version (Version(..), showVersion)
 import qualified Data.Aeson as A
 import qualified Data.ByteString.Lazy.Char8 as BL
 
 import Options.Applicative hiding (str)
 
 import qualified Paths_purescript as Paths
-import Language.PureScript.Publish (preparePackage)
+import Language.PureScript.Publish
+
+dryRun :: Parser Bool
+dryRun = switch $
+     long "dry-run"
+  <> help "Produce no output, and don't require a tagged version to be checked out."
 
 main :: IO ()
-main = execParser opts >> publish
+main = execParser opts >>= publish
   where
-  opts        = info (version <*> helper) infoModList
+  opts        = info (version <*> helper <*> dryRun) infoModList
   infoModList = fullDesc <> headerInfo <> footerInfo
   headerInfo  = header "psc-publish - Generates documentation packages for upload to http://pursuit.purescript.org"
   footerInfo  = footer $ "psc-publish " ++ showVersion Paths.version
@@ -21,8 +26,13 @@ main = execParser opts >> publish
   version :: Parser (a -> a)
   version = abortOption (InfoMsg (showVersion Paths.version)) $ long "version" <> help "Show the version number" <> hidden
 
-publish :: IO ()
-publish = do
-  pkg <- preparePackage
-  BL.putStrLn (A.encode pkg)
-
+publish :: Bool -> IO ()
+publish isDryRun =
+  if isDryRun
+    then do
+      let dummyVersion = ("0.0.0", Version [0,0,0] [])
+      _ <- preparePackage $ defaultPublishOptions { publishGetVersion = return dummyVersion }
+      putStrLn "Dry run completed, no errors."
+    else do
+      pkg <- preparePackage defaultPublishOptions
+      BL.putStrLn (A.encode pkg)
