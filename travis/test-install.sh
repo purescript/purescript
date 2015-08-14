66e78d89157b47adad9e991b641b23efa8b0ec40
Oleg Grenrus <oleg.grenrus@iki.fi>
2015-08-14 11:04:23 +0300

    Reuse sandbox in test-install.sh

--- travis/test-install.sh
+++ travis/test-install.sh
@@ -6,11 +6,12 @@ cabal sdist
 if SRC_TGZ="$PWD/dist/$(cabal info . | awk '{print $2;exit}').tar.gz"
 then
   if [ "$RUNSDISTTESTS" = "YES" ]; then
-	mkdir test-install
-	cd test-install
-	tar --strip-components=1 -xzf $SRC_TGZ
-	cabal install --enable-tests --force-reinstalls
-	cabal test
+    mkdir test-install
+    cd test-install
+    tar --strip-components=1 -xzf $SRC_TGZ
+    cabal sandbox init --sandbox ../.cabal-sandbox
+    cabal install --enable-tests --force-reinstalls
+    cabal test
   else
     cabal install "$SRC_TGZ"
   fi
