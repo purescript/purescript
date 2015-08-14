beb055654321ac62a4bc3816db53b9ab6b671eca
Oleg Grenrus <oleg.grenrus@iki.fi>
2015-08-14 07:05:06 +0300

    Test sdist installation

--- travis/test-install.sh
+++ travis/test-install.sh
@@ -1,13 +1,21 @@
 set -e
 
 # Check that a source distribution can be successfully generated, and that
-# the generated source distribution can be installed
+# the generated source distribution can be installed and tested
 cabal sdist
-if SRC_TGZ="$PWD/dist/$(cabal info . | awk '{print $2;exit}').tar.gz"
+PKGNAME=$(cabal info . | awk '{print $2;exit}')
+if SRC_TGZ="$PWD/dist/$PKGNAME.tar.gz"
 then
   mkdir -p ../install-test
   cd ../install-test
   mkdir -p sandboxes/$GHCVER/${STACKAGE:-none}
   cabal sandbox init --sandbox sandboxes/$GHCVER/${STACKAGE:-none}
-  cabal install -j2 --ghc-options='+RTS -A32m -RTS' "$SRC_TGZ"
+
+  if [ `echo "$STACKAGE" | sed 's/\..*$//'` = "lts-3" ]; then
+	tar -xzf i--strip-components=1 $PKGNAME.tar.gz
+	cabal install
+	cabal test
+  else
+    cabal install "$SRC_TGZ"
+  fi
 fi
