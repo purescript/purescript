3652252002d13d4707a6fef1a7a96195eaa2c031
Phil Freeman <paf31@cantab.net>
2015-09-10 17:58:15 -0700

    Support constraints without forall

---
+++ examples/passing/NakedConstraint.purs
@@ -0,0 +1,12 @@
+module Main where
+
+import Control.Monad.Eff.Console
+
+class Partial where
+
+data List a = Nil | Cons a (List a)
+
+head :: (Partial) => List Int -> Int
+head (Cons x _) = x
+
+main = log "Done"
