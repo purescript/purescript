678e9f6f11b1b4b05a09279985495ed14e30cc43
rightfold <rightfold@gmail.com>
2016-04-26 19:44:04 +0200

    Use Test.Assert in PassThroughCases optimization test

--- examples/passing/PassThroughCases.purs
+++ examples/passing/PassThroughCases.purs
@@ -1,9 +1,10 @@
 module Main where
 
-import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (log, error)
-import qualified Data.Maybe as Maybe
+import Control.Monad.Eff.Console (log)
+import Data.Maybe as Maybe
+import Prelude
+import Test.Assert (assert)
 
 foreign import same :: forall eff a. a -> a -> Eff eff Boolean
 foreign import exit :: forall eff. Eff eff Unit
@@ -18,32 +19,27 @@ main = do
   let just = Maybe.Just 1
 
   aEq <- same c (case c of C -> C)
-  if aEq then log "aEq ok" else error "aEq fail"
+  assert (aEq)
 
   dEq1 <- same d (case d of D x -> D x)
-  if dEq1 then log "dEq1 ok" else error "dEq1 fail"
+  assert (dEq1)
 
   dEq2 <- same d (case d of D x -> D 2)
-  if not dEq2 then log "dEq2 ok" else error "dEq2 fail"
+  assert (not dEq2)
 
   eEq1 <- same e (case e of E x y -> E x y)
-  if eEq1 then log "eEq1 ok" else error "eEq1 fail"
+  assert (eEq1)
 
   eEq2 <- same e (case e of E x y -> E y x)
-  if not eEq2 then log "eEq2 ok" else error "eEq2 fail"
+  assert (not eEq2)
 
   nothingEq <- same nothing (case nothing of Maybe.Nothing -> Maybe.Nothing)
-  if nothingEq then log "nothing ok" else error "nothing fail"
+  assert (nothingEq)
 
   justEq1 <- same just (case just of Maybe.Just x -> Maybe.Just x)
-  if justEq1 then log "justEq1 ok" else error "justEq1 fail"
+  assert (justEq1)
 
   justEq2 <- same just (case just of Maybe.Just x -> Maybe.Just 2)
-  if not justEq2 then log "justEq2 ok" else error "justEq2 fail"
+  assert (not justEq2)
 
-  let ok = aEq
-           && dEq1 && not dEq2
-           && eEq1 && not eEq2
-           && nothingEq
-           && justEq1 && not justEq2
-  if ok then log "Done" else exit
+  log "Success!"
