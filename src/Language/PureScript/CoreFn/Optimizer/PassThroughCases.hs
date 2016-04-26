0c7cfb47496d95d03e9f49f6699b03def58630c5
rightfold <rightfold@gmail.com>
2016-04-26 18:56:29 +0200

    Make dissectConstruction function less trigger-happy
    Before, this function would return true for any call where the callee is
    a variable. After this change, it only returns true for actual data
    constructor calls.

--- src/Language/PureScript/CoreFn/Optimizer/PassThroughCases.hs
+++ src/Language/PureScript/CoreFn/Optimizer/PassThroughCases.hs
@@ -13,6 +13,7 @@ module Language.PureScript.CoreFn.Optimizer.PassThroughCases
 import Language.PureScript.CoreFn.Ann (Ann)
 import Language.PureScript.CoreFn.Binders (Binder(..))
 import Language.PureScript.CoreFn.Expr
+import Language.PureScript.CoreFn.Meta (Meta(IsConstructor))
 import Language.PureScript.CoreFn.Module (Module, moduleDecls)
 import Language.PureScript.CoreFn.Traversals (everywhereOnValues)
 import Language.PureScript.Names
@@ -54,3 +55,27 @@ passThroughCases m = m { moduleDecls = onBinds (moduleDecls m) }
 
   qv :: Qualified Ident
   qv = Qualified Nothing v
+
+
+-- |
+-- Return the constructor and arguments from what looks like a data constructor
+-- call.
+--
+dissectConstruction :: Expr Ann -> Maybe (Qualified Ident, [Expr Ann])
+dissectConstruction e = (,) <$> findConstructor e <*> findArguments e
+  where
+  findConstructor :: Expr Ann -> Maybe (Qualified Ident)
+  findConstructor (App _ (Var ann c) _)
+    | isConstructor ann = Just c
+    | otherwise         = Nothing
+  findConstructor (App _ c _) = findConstructor c
+  findConstructor _ = Nothing
+
+  findArguments :: Expr Ann -> Maybe [Expr Ann]
+  findArguments (App _ (Var _ _) a) = Just [a]
+  findArguments (App _ c a) = (++ [a]) <$> findArguments c
+  findArguments _ = Nothing
+
+  isConstructor :: Ann -> Bool
+  isConstructor (_, _, _, Just (IsConstructor _ _)) = True
+  isConstructor _ = False
