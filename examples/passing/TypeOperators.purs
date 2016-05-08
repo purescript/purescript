7a3b7093414edc1e32de5e930e8871a040e08f60
Phil Freeman <paf31@cantab.net>
2016-05-07 22:04:51 -0700

    Fix #2092, precedence of type application

--- examples/passing/TypeOperators.purs
+++ examples/passing/TypeOperators.purs
@@ -5,6 +5,11 @@ import A (type (~>), type (/\), (/\))
 natty ∷ ∀ f. f ~> f
 natty x = x
 
+data Compose f g a = Compose (f (g a))
+
+testPrecedence ∷ ∀ f g. Compose f g ~> Compose f g
+testPrecedence x = x
+
 swap ∷ ∀ a b. a /\ b → b /\ a
 swap (a /\ b) = b /\ a
 
