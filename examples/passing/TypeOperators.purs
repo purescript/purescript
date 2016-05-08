437eedc993f651becda437ce26faf602e8902243
Phil Freeman <paf31@cantab.net>
2016-05-07 22:18:43 -0700

    one more test

--- examples/passing/TypeOperators.purs
+++ examples/passing/TypeOperators.purs
@@ -7,8 +7,11 @@ natty x = x
 
 data Compose f g a = Compose (f (g a))
 
-testPrecedence ∷ ∀ f g. Compose f g ~> Compose f g
-testPrecedence x = x
+testPrecedence1 ∷ ∀ f g. Compose f g ~> Compose f g
+testPrecedence1 x = x
+
+testPrecedence2 ∷ ∀ f g. f ~> g → f ~> g
+testPrecedence2 nat fx = nat fx
 
 swap ∷ ∀ a b. a /\ b → b /\ a
 swap (a /\ b) = b /\ a
