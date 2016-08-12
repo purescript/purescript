ecdfc98c3a075a4ee4b6e6074f6232bca8100747
FrigoEU <simon.van.casteren@gmail.com>
2016-08-12 12:51:40 +0200

    Improved efficiency of finding missing/additional properties

--- src/Language/PureScript/TypeChecker/Subsumption.hs
+++ src/Language/PureScript/TypeChecker/Subsumption.hs
@@ -7,10 +7,11 @@ module Language.PureScript.TypeChecker.Subsumption
 
 import Prelude.Compat
 
+import Control.Monad (when)
 import Control.Monad.Error.Class (MonadError(..))
 import Control.Monad.State.Class (MonadState(..), gets)
 
-import Data.List (sortBy)
+import Data.List (sortBy, uncons)
 import Data.Ord (comparing)
 
 import Language.PureScript.AST
@@ -22,9 +23,11 @@ import Language.PureScript.TypeChecker.Skolems
 import Language.PureScript.TypeChecker.Unify
 import Language.PureScript.Types
 
+
 -- | Check that one type subsumes another, rethrowing errors to provide a better error message
 subsumes :: (MonadError MultipleErrors m, MonadState CheckState m) => Maybe Expr -> Type -> Type -> m (Maybe Expr)
-subsumes val ty1 ty2 = withErrorMessageHint (ErrorInSubsumption ty1 ty2) $ subsumes' val ty1 ty2
+subsumes val ty1 ty2 = do 
+  withErrorMessageHint (ErrorInSubsumption ty1 ty2) $ subsumes' val ty1 ty2
 
 -- | Check tahat one type subsumes another
 subsumes' :: (MonadError MultipleErrors m, MonadState CheckState m) =>
@@ -60,6 +63,10 @@ subsumes' val (TypeApp f1 r1) (TypeApp f2 r2) | f1 == tyRecord && f2 == tyRecord
     (ts2, r2') = rowToList r2
     ts1' = sortBy (comparing fst) ts1
     ts2' = sortBy (comparing fst) ts2
+  when (r1' == REmpty) 
+    (maybe (return ()) (throwError . errorMessage . PropertyIsMissing . fst) (firstMissingProp ts2' ts1'))
+  when (r2' == REmpty) 
+    (maybe (return ()) (throwError . errorMessage . AdditionalProperty . fst) (firstMissingProp ts1' ts2'))
   go ts1' ts2' r1' r2'
   return val
   where
@@ -68,10 +75,6 @@ subsumes' val (TypeApp f1 r1) (TypeApp f2 r2) | f1 == tyRecord && f2 == tyRecord
   go ((p1, ty1) : ts1) ((p2, ty2) : ts2) r1' r2'
     | p1 == p2 = do _ <- subsumes Nothing ty1 ty2
                     go ts1 ts2 r1' r2'
-    | p1 /= p2 && r1' == REmpty && p2 `notElem` map fst ts1 = 
-        throwError . errorMessage $ PropertyIsMissing p2
-    | p1 /= p2 && r2' == REmpty && p1 `notElem` map fst ts2 = 
-        throwError . errorMessage $ AdditionalProperty p1
     | p1 < p2 = do rest <- freshType
                    -- What happens next is a bit of a hack.
                    -- TODO: in the new type checker, object properties will probably be restricted to being monotypes
@@ -81,7 +84,27 @@ subsumes' val (TypeApp f1 r1) (TypeApp f2 r2) | f1 == tyRecord && f2 == tyRecord
     | otherwise = do rest <- freshType
                      unifyTypes r1' (RCons p2 ty2 rest)
                      go ((p1, ty1) : ts1) ts2 rest r2'
+
 subsumes' val ty1 ty2@(TypeApp obj _) | obj == tyRecord = subsumes val ty2 ty1
 subsumes' val ty1 ty2 = do
   unifyTypes ty1 ty2
   return val
+
+firstMissingProp :: forall a b. Ord a => [(a, b)] -> [(a, b)] -> Maybe (a, b)
+firstMissingProp t2 t1 = fst <$> (uncons $ minusBy (comparing fst) t2 t1)
+
+-- |  The 'minusBy' function computes the difference of two ordered lists.
+-- The result consists of elements from the first list that do not appear
+-- in the second list.  If the first input is a set, then the output is
+-- a set.
+--
+minusBy :: (a -> b -> Ordering) -> [a] -> [b] -> [a]
+minusBy cmp = loop
+  where
+     loop [] _ys = []
+     loop xs [] = xs
+     loop (x:xs) (y:ys)
+       = case cmp x y of
+          LT -> x : loop xs (y:ys)
+          EQ ->     loop xs (y:ys)
+          GT ->     loop (x:xs) ys
