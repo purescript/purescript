9c6a7852ac6a47794b70b37a525272c7392111dc
Brandon Hamilton <brandon.hamilton@gmail.com>
2016-10-11 23:39:14 +0200

    Update data constructor example

--- examples/passing/DctorName.purs
+++ examples/passing/DctorName.purs
@@ -9,16 +9,25 @@ data Foo' = Foo' Bar'
 
 data Baz'' = Baz'' | Baz'
 
-f :: Foo' -> Boolean
-f a = case a of Foo' b -> true
+f ∷ Foo' → Boolean
+f a = case a of Foo' b → true
 
-g :: Baz'' -> Int
+f' ∷ Boolean
+f' = f $ Foo' $ Bar' 0
+
+g ∷ Baz'' → Int
 g Baz'' = 0
 g Baz' = 1
- 
-h :: Bar' -> Int
+
+g' ∷ Int
+g' = g Baz''
+
+h ∷ Bar' → Int
 h (Bar' x)
  | x <= 10 = x * 2 
  | otherwise = 10
+
+h' ∷ Int
+h' = h $ Bar' 4
 
 main = log "Done"
