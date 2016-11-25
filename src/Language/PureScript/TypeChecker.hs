51c01714571e1204b0be425092834debd480b84b
Liam Goodacre <goodacre.liam@gmail.com>
2016-11-25 21:18:45 +0000

    Update error message of `ErrorInDataBindingGroup` to include participating identifiers.

--- src/Language/PureScript/TypeChecker.hs
+++ src/Language/PureScript/TypeChecker.hs
@@ -16,6 +16,7 @@ import Control.Monad.Error.Class (MonadError(..))
 import Control.Monad.State.Class (MonadState(..), modify)
 import Control.Monad.Supply.Class (MonadSupply)
 import Control.Monad.Writer.Class (MonadWriter(..))
+import Control.Lens ((^..), _1, _2)
 
 import Data.Foldable (for_, traverse_)
 import Data.List (nub, nubBy, (\\), sort, group)
@@ -205,9 +206,10 @@ typeCheckAll moduleName _ = traverse go
       addDataType moduleName dtype name args' dctors ctorKind
     return $ DataDeclaration dtype name args dctors
   go (d@(DataBindingGroupDeclaration tys)) = do
-    warnAndRethrow (addHint ErrorInDataBindingGroup) $ do
-      let syns = mapMaybe toTypeSynonym tys
-      let dataDecls = mapMaybe toDataDecl tys
+    let syns = mapMaybe toTypeSynonym tys
+    let dataDecls = mapMaybe toDataDecl tys
+    let bindingGroupNames = (syns^..traverse._1) ++ (dataDecls^..traverse._2)
+    warnAndRethrow (addHint (ErrorInDataBindingGroup bindingGroupNames)) $ do
       (syn_ks, data_ks) <- kindsOfAll moduleName syns (map (\(_, name, args, dctors) -> (name, args, concatMap snd dctors)) dataDecls)
       for_ (zip dataDecls data_ks) $ \((dtype, name, args, dctors), ctorKind) -> do
         when (dtype == Newtype) $ checkNewtype name dctors
