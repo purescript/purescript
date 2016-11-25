ae98dfafa9a20f93e26f1fc0e6bca813f50ea1f8
Tim McGilchrist <timmcgil@gmail.com>
2016-11-26 09:20:13 +1100

    Exhaustive pattern match on JSAnnot. (#2432)
    * Exhaustive pattern match on JSAnnot.
    
    Also append to contributors file.
    
    * Bump LTS versions for stack.

--- src/Language/PureScript/Bundle.hs
+++ src/Language/PureScript/Bundle.hs
@@ -534,6 +534,7 @@ codeGen optionsMainModule optionsNamespace ms =  renderToString (JSAstProgram (p
     where
     squash JSNoAnnot = JSAnnot (TokenPn 0 0 2) []
     squash (JSAnnot pos ann) = JSAnnot (keepCol pos) (map splat ann)
+    squash JSAnnotSpace = JSAnnot (TokenPn 0 0 2) []
 
     splat (CommentA pos s) = CommentA (keepCol pos) s
     splat (WhiteSpace pos w) = WhiteSpace (keepCol pos) w
@@ -589,6 +590,7 @@ codeGen optionsMainModule optionsNamespace ms =  renderToString (JSAstProgram (p
 
       appendAnn a JSNoAnnot = JSAnnot tokenPosnEmpty [a]
       appendAnn a (JSAnnot _ anns) = JSAnnot tokenPosnEmpty (a:anns ++ [WhiteSpace tokenPosnEmpty "  "])
+      appendAnn a JSAnnotSpace = JSAnnot tokenPosnEmpty [a]
 
   runMain :: String -> [JSStatement]
   runMain mn =
