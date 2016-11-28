0ccb13f6687c33327b24207062e1de0c02da575d
Michael Ficarra <git@michael.ficarra.me>
2016-11-28 16:59:15 -0500

    add descriptions to StringEscape assertions; add one more assertion

--- examples/passing/StringEscapes.purs
+++ examples/passing/StringEscapes.purs
@@ -1,7 +1,7 @@
 module Main where
 
-import Prelude ((==), (<>), bind)
-import Test.Assert (assert)
+import Prelude ((==), (/=), (<>), bind)
+import Test.Assert (assert, assert')
 import Control.Monad.Eff.Console (log)
 
 singleCharacter = "\0\b\t\n\v\f\r\"\\" == "\x0\x8\x9\xA\xB\xC\xD\x22\x5C"
@@ -12,12 +12,15 @@ highSurrogate = "\xD834"
 lowSurrogate = "\xDF06"
 loneSurrogates = (highSurrogate <> lowSurrogate) == "\x1D306"
 outOfOrderSurrogates = (lowSurrogate <> highSurrogate) == "\xDF06\xD834"
+replacement = "\xFFFD"
+notReplacing = replacement /= highSurrogate
 
 main = do
-  assert singleCharacter
-  assert hex
-  assert decimal
-  assert surrogatePair
-  assert loneSurrogates
-  assert outOfOrderSurrogates
+  assert' "single-character escape sequences" singleCharacter
+  assert' "hex escape sequences" hex
+  assert' "decimal escape sequences" decimal
+  assert' "astral code points are represented as a UTF-16 surrogate pair" surrogatePair
+  assert' "lone surrogates may be combined into a surrogate pair" loneSurrogates
+  assert' "lone surrogates may be combined out of order to remain lone surrogates" outOfOrderSurrogates
+  assert' "lone surrogates are not replaced with the Unicode replacement character U+FFFD" notReplacing
   log "Done"
