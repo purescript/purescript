purescript/purescript

commit 92c8c653f42801bc68de212b755f616c940d5b2c

Use Text over String (#2418)
* Lexer done.

* Names done.

* Parser.Common done.

* CodeGen.JS.Common done.

* Constants done.

* Environment done.

* Types done.

* Parser.Types done.

* Externs done.

* Codegen.JS.AST done.

* CodeGen.JS.Optimizer.Common done.

* Monad.Supply.Class done.

* CodeGen.JS.Optimizer.Inliner done.

* CodeGen.JS.Optimizer.TCO done.

* don't change inliner semantics

* project specific Prelude

* Pretty.Common done.

* Pretty.Types done.

* Pretty.Values done.

* Pretty.JS done.

* Pretty.Common done.

* Pretty.JS now on Text bases emit.

* fix warning

* AST.SourcePos done.

* Pretty.Kinds done.

* Pretty.Common more fixes.

* AST.Literals done.

* CoreFn.Expr done.

* Pretty.Types workaround

* Pretty.Values done.

* Errors done.

* AST.Declarations

* Parser.Declarations done.

* TypChecker.Monad done.

* Sugar.ObjectWildcards done.

* Linter.Exhaustive done.

* Linter.Imports done.

* TypeChecker.Kinds done.

* Sugar.TypeClasses done.

* TypeChecker Skolems

* TypeChecker.Unify done.

* Renamer

* CodeGen.JS done.

* TypeChecker.Entailment done.

* TypeChecker.Types done.

* CoreFn.ToJSON done.

* Linter done.

* TypeChecker done.

* Sugar.TypeClasses.Deriving done.

* Make done. This needs serious review

* Errors.JSON done.

* Docs.RenderedCode.Types done.

* Interactive.Printer done.

* Interactive.Completion done.

* Interactive.Parser done.

* Docs.RenderedCode.Render done.

* Docs.Types done.

* Docs.Render done.

* Docs.Convert.ReExports done.

* Docs.Convert.Single done.

* Docs.Convert.ReExports done.

* Docs.Convert done.

* Docs.AsMarkdown done.

* Ide done.

* Ide.Imports done.

* Interactive done.

* executables done.

* Test Mains

* ReexportsSpec

* add Prelude module to cabal file

* asString -> asText for module names

* simpler imports for Supply.Class

src/Language/PureScript/Linter.hs
@@ -11,6 +11,7 @@ import Data.List (nub, (\\))
 import Data.Maybe (mapMaybe)
 import Data.Monoid
 import qualified Data.Set as S
+import Data.Text (Text)
 
 import Language.PureScript.AST
 import Language.PureScript.Crash
@@ -81,10 +82,10 @@ lint (Module _ _ mn ds _) = censor (addHint (ErrorInModule mn)) $ mapM_ lintDecl
   checkTypeVars :: S.Set String -> Type -> MultipleErrors
   checkTypeVars set ty = everythingWithContextOnTypes set mempty mappend step ty <> findUnused ty
     where
-    step :: S.Set String -> Type -> (S.Set String, MultipleErrors)
+    step :: S.Set Text -> Type -> (S.Set Text, MultipleErrors)
     step s (ForAll tv _ _) = bindVar s tv
     step s _ = (s, mempty)
-    bindVar :: S.Set String -> String -> (S.Set String, MultipleErrors)
+    bindVar :: S.Set Text -> Text -> (S.Set Text, MultipleErrors)
     bindVar = bind ShadowedTypeVar
     findUnused :: Type -> MultipleErrors
     findUnused ty' =
@@ -93,7 +94,7 @@ lint (Module _ _ mn ds _) = censor (addHint (ErrorInModule mn)) $ mapM_ lintDecl
           unused = nub declared \\ nub used
       in foldl (<>) mempty $ map (errorMessage . UnusedTypeVar) unused
       where
-      go :: Type -> [String]
+      go :: Type -> [Text]
       go (ForAll tv _ _) = [tv]
       go _ = []
 

commit 4435d97adc6bdd7884468aa9fb8dc5b3a2f7bccd

fix regressions introduced by #2418

src/Language/PureScript/Errors.hs
@@ -972,7 +972,7 @@ prettyPrintSingleError (PPEOptions codeColor full level showWiki) e = flip evalS
             ]
     renderHint (ErrorInDataBindingGroup nms) detail =
       paras [ detail
-            , line $ "in data binding group " ++ intercalate ", " (map runProperName nms)
+            , line $ "in data binding group " <> T.intercalate ", " (map runProperName nms)
             ]
     renderHint (ErrorInTypeSynonym name) detail =
       paras [ detail
@@ -988,7 +988,7 @@ prettyPrintSingleError (PPEOptions codeColor full level showWiki) e = flip evalS
             ]
     renderHint (ErrorInTypeClassDeclaration name) detail =
       paras [ detail
-            , line $ "in type class declaration for " ++ markCode (runProperName name)
+            , line $ "in type class declaration for " <> markCode (runProperName name)
             ]
     renderHint (ErrorInForeignImport nm) detail =
       paras [ detail

src/Language/PureScript/Linter.hs
@@ -48,7 +48,7 @@ lint (Module _ _ mn ds _) = censor (addHint (ErrorInModule mn)) $ mapM_ lintDecl
     f (TypeClassDeclaration name args _ _ decs) = addHint (ErrorInTypeClassDeclaration name) (foldMap (f' (S.fromList $ fst <$> args)) decs)
     f dec = f' S.empty dec
 
-    f' :: S.Set String -> Declaration -> MultipleErrors
+    f' :: S.Set Text -> Declaration -> MultipleErrors
     f' s (PositionedDeclaration pos _ dec) = addHint (PositionedError pos) (f' s dec)
     f' s dec@(ValueDeclaration name _ _ _) = addHint (ErrorInValueDeclaration name) (warningsInDecl moduleNames dec <> checkTypeVarsInDecl s dec)
     f' s (TypeDeclaration name ty) = addHint (ErrorInTypeDeclaration name) (checkTypeVars s ty)
@@ -76,10 +76,10 @@ lint (Module _ _ mn ds _) = censor (addHint (ErrorInModule mn)) $ mapM_ lintDecl
            | otherwise = mempty
     stepDo _ _ = mempty
 
-  checkTypeVarsInDecl :: S.Set String -> Declaration -> MultipleErrors
+  checkTypeVarsInDecl :: S.Set Text -> Declaration -> MultipleErrors
   checkTypeVarsInDecl s d = let (f, _, _, _, _) = accumTypes (checkTypeVars s) in f d
 
-  checkTypeVars :: S.Set String -> Type -> MultipleErrors
+  checkTypeVars :: S.Set Text -> Type -> MultipleErrors
   checkTypeVars set ty = everythingWithContextOnTypes set mempty mappend step ty <> findUnused ty
     where
     step :: S.Set Text -> Type -> (S.Set Text, MultipleErrors)