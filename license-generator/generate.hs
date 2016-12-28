9ed097452bacc512049b2003739896aa511d9454
Harry Garrood <harry@garrood.me>
2016-12-28 03:22:57 +0000

    More robust license generator script
    Changes:
    
    * Ensure that all responses are 200 OK responses, i.e. avoids errors
      such as a 404 error page being treated as the actual license file.
    * Use the license-file field of the package's Cabal file -- most
      packages simply use LICENSE as their license-file, but not all do,
      and this is safer than the previous approach of maintaining a list
      of packages that deviate from the convention of using LICENSE.
    * Exits with a nonzero code if at least one dependency returned a
      non-200 response for its license file.
    * Uses the license files from the actual versions of dependencies which
      we are using instead of the very latest one.
    * Use http-client instead of curl (mainly for the response code checking
      but this has the nice side effect that the script should work
      basically anywhere now).
    
    Packages who list their 'license' field as PublicDomain (at the time of
    writing, this is just monad-loops) will not be treated as failures if a
    license-file is not found.

--- license-generator/generate.hs
+++ license-generator/generate.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TupleSections #-}
 -- |
 -- A small script which regenerates the LICENSE file with all
 -- dependencies' licenses, when the dependencies are provided via standard
@@ -5,42 +6,167 @@
 --
 -- It is recommended to run this as follows:
 --
--- stack list-dependencies | cut -f 1 -d ' ' | stack exec runhaskell license-generator/generate.hs > LICENSE
+-- stack list-dependencies | stack exec runhaskell license-generator/generate.hs > LICENSE
 --
 
 module Main (main) where
 
-import Control.Monad (forM_)
-import Data.Char (isSpace)
+import Control.Monad (forM_, when)
+import Data.Char (isSpace, toLower)
+import Data.Maybe (mapMaybe)
 import Data.List
-import System.Process
+import Data.List.Split (splitOn)
+import Data.Foldable
+import Data.Traversable
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Encoding as TLE
+import Network.HTTP.Types (ok200)
+import Network.HTTP.Client (Manager, newManager, httpLbs, parseRequest, responseBody, responseStatus)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
 import System.IO (hPutStrLn, stderr, getContents)
-
-echoHeader :: IO ()
-echoHeader =
-    readFile "license-generator/header.txt" >>= putStr
-
-depsNames :: IO [String]
-depsNames =
-    fmap (filter (/= "purescript") . lines) getContents
-
-depsLicense :: String -> IO ()
-depsLicense dep = do
-    let licenseFile = if dep == "Glob" then "LICENSE.txt" else "LICENSE"
-    hPutStrLn stderr dep
-    license <- readProcess "curl" ["--silent", "https://hackage.haskell.org/package/" ++ dep ++ "/src/" ++ licenseFile] ""
-    putStrLn $ dep ++ " LICENSE file:"
-    putStrLn ""
-    putStrLn $ f license
-  where
-    f = unlines . map (trimEnd . ("  " ++)) . lines
-    trimEnd = reverse . dropWhile isSpace . reverse
+import System.Exit (exitFailure)
 
 main :: IO ()
 main = do
-    deps <-depsNames
-    echoHeader
-    putStrLn ""
-    forM_ deps $ \d -> putStr "  " >> putStrLn d
-    putStrLn ""
-    forM_ deps depsLicense
+  deps <- depsNamesAndVersions
+  echoHeader
+  putStrLn ""
+  forM_ deps $ \(d, _) -> putStr "  " >> putStrLn d
+  putStrLn ""
+  manager <- newManager tlsManagerSettings
+  results <- traverse (\d -> (d,) <$> depsLicense manager d) deps
+  let failures = filter (not . snd) results
+  if not (null failures)
+    then do
+      hPutStrLn stderr "Licenses were not found for the following packages:"
+      traverse_ (hPutStrLn stderr . showPair . fst) failures
+      exitFailure
+    else
+      hPutStrLn stderr "Everything looks ok."
+
+  where
+  showPair (pkg, version) = pkg ++ " " ++ version
+
+echoHeader :: IO ()
+echoHeader =
+  readFile "license-generator/header.txt" >>= putStr
+
+depsNamesAndVersions :: IO [(String, String)]
+depsNamesAndVersions = do
+  contents <- lines <$> getContents
+  deps <- traverse parse contents
+  pure (filter ((/= "purescript") . fst) deps)
+
+  where
+  parse line =
+    case splitOn " " line of
+      [pkg, vers] -> pure (pkg, vers)
+      _ -> fail $ "Unable to parse input line: " ++ line
+
+-- Returns True on success, False on failure.
+depsLicense :: Manager -> (String, String) -> IO Bool
+depsLicense manager dep = do
+  hPutStrLn stderr (fst dep)
+  result <- downloadLicenseFromHackage manager dep
+  case result of
+    FoundLicense license -> do
+      putStrLn $ fst dep ++ " LICENSE file:"
+      putStrLn ""
+      putStrLn $ f license
+      pure True
+    LicenseNotNeeded ->
+      pure True
+    Failed ->
+      pure False
+  where
+  f = unlines . map (trimEnd . ("  " ++)) . lines
+  trimEnd = reverse . dropWhile isSpace . reverse
+
+data LicenseResult
+  = FoundLicense String
+  | LicenseNotNeeded
+  | Failed
+  deriving (Show, Eq, Ord)
+
+downloadLicenseFromHackage :: Manager -> (String, String) -> IO LicenseResult
+downloadLicenseFromHackage manager dep = do
+  mcabalFile <- downloadCabalFileFromHackage manager dep
+  case mcabalFile of
+    Nothing ->
+      pure Failed
+    Just cabalFile ->
+      let
+        field f = extractCabalField f cabalFile
+      in
+        case (field "license", field "license-file") of
+          (_, Just licenseFile) -> do
+            getLicense licenseFile
+          (Just "PublicDomain", _) -> do
+            pure LicenseNotNeeded
+          _ -> do
+            hPutStrLn stderr $
+              "Unable to extract license information from cabal file for " ++
+              fst dep
+            pure Failed
+
+  where
+  getLicense licenseFile = do
+    r <- downloadFromHackage ("/src/" ++ licenseFile) manager dep
+    pure $ maybe Failed FoundLicense r
+
+-- Attempt to extract a field from a cabal file. Note that this only works for
+-- fields which are at the top level, not inside subsections such as
+-- 'executable' or 'test-suite'.
+extractCabalField :: String -> String -> Maybe String
+extractCabalField fieldName cabalFile =
+  case mapMaybe (stripPrefixCaseInsensitive fieldName) (lines cabalFile) of
+    [line] ->
+      Just $
+        line
+        |> dropWhile isSpace
+        |> drop 1 -- colon
+        |> trim
+    _ ->
+      Nothing
+  where
+  x |> f = f x
+
+  trim =
+    reverse . dropWhile isSpace . reverse . dropWhile isSpace
+
+  stripPrefixCaseInsensitive prefix str =
+    if map toLower prefix `isPrefixOf` map toLower str
+      then Just (drop (length prefix) str)
+      else Nothing
+
+downloadCabalFileFromHackage :: Manager -> (String, String) -> IO (Maybe String)
+downloadCabalFileFromHackage manager dep = do
+  downloadFromHackage ("/src/" ++ fst dep ++ ".cabal") manager dep
+
+downloadFromHackage :: String -> Manager -> (String, String) -> IO (Maybe String)
+downloadFromHackage urlpath manager dep = do
+  let url = hackageBaseUrl dep ++ urlpath
+  req <- parseRequest url
+  resp <- httpLbs req manager
+
+  let status = responseStatus resp
+  if (status /= ok200)
+    then do
+      hPutStrLn stderr $ "Bad status code for " ++ url
+      hPutStrLn stderr $ "Expected 200, got " ++ show status
+      pure Nothing
+    else
+      pure (Just (toString (responseBody resp)))
+
+  where
+  toString = TL.unpack . TLE.decodeUtf8
+
+hackageBaseUrl :: (String, String) -> String
+hackageBaseUrl (dep, version) =
+  concat
+    [ "https://hackage.haskell.org/package/"
+    , dep
+    , "-"
+    , version
+    ]
