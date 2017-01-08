49235c02923dd4de3ad76c4c4d96a2b6463e0632
Phil Freeman <paf31@cantab.net>
2017-01-07 18:27:49 -0800

    Avoid shadowed names

--- src/Language/PureScript/TypeChecker/Synonyms.hs
+++ src/Language/PureScript/TypeChecker/Synonyms.hs
@@ -47,25 +47,25 @@ replaceAllTypeSynonyms' syns = traverseWithoutLooping try
   go c args (TypeApp f arg) = go (c + 1) (arg : args) f
   go _ _ _ = return Nothing
 
-  -- This is a variant on 'everywhereOnTypesTopDownM', but which does not recur into
-  -- the left hand side of 'ExpandedSynonym', to avoid an infinite loop.
-  --
-  -- TODO: improve this later
-  traverseWithoutLooping :: Monad m => (Type -> m Type) -> Type -> m Type
-  traverseWithoutLooping f = go <=< f
-    where
-    go (TypeApp t1 t2) = TypeApp <$> (f t1 >>= go) <*> (f t2 >>= go)
-    go (ForAll arg ty sco) = ForAll arg <$> (f ty >>= go) <*> pure sco
-    go (ConstrainedType cs ty) = ConstrainedType <$> mapM (overConstraintArgs (mapM (go <=< f))) cs <*> (f ty >>= go)
-    go (RCons name ty rest) = RCons name <$> (f ty >>= go) <*> (f rest >>= go)
-    go (KindedType ty k) = KindedType <$> (f ty >>= go) <*> pure k
-    go (PrettyPrintFunction t1 t2) = PrettyPrintFunction <$> (f t1 >>= go) <*> (f t2 >>= go)
-    go (PrettyPrintObject t) = PrettyPrintObject <$> (f t >>= go)
-    go (PrettyPrintForAll args t) = PrettyPrintForAll args <$> (f t >>= go)
-    go (BinaryNoParensType t1 t2 t3) = BinaryNoParensType <$> (f t1 >>= go) <*> (f t2 >>= go) <*> (f t3 >>= go)
-    go (ParensInType t) = ParensInType <$> (f t >>= go)
-    go (ExpandedSynonym t1 t2) = ExpandedSynonym t1 <$> (f t2 >>= go)
-    go other = f other
+-- This is a variant on 'everywhereOnTypesTopDownM', but which does not recur into
+-- the left hand side of 'ExpandedSynonym', to avoid an infinite loop.
+--
+-- TODO: improve this later
+traverseWithoutLooping :: Monad m => (Type -> m Type) -> Type -> m Type
+traverseWithoutLooping f = go <=< f
+  where
+  go (TypeApp t1 t2) = TypeApp <$> (f t1 >>= go) <*> (f t2 >>= go)
+  go (ForAll arg ty sco) = ForAll arg <$> (f ty >>= go) <*> pure sco
+  go (ConstrainedType cs ty) = ConstrainedType <$> mapM (overConstraintArgs (mapM (go <=< f))) cs <*> (f ty >>= go)
+  go (RCons name ty rest) = RCons name <$> (f ty >>= go) <*> (f rest >>= go)
+  go (KindedType ty k) = KindedType <$> (f ty >>= go) <*> pure k
+  go (PrettyPrintFunction t1 t2) = PrettyPrintFunction <$> (f t1 >>= go) <*> (f t2 >>= go)
+  go (PrettyPrintObject t) = PrettyPrintObject <$> (f t >>= go)
+  go (PrettyPrintForAll args t) = PrettyPrintForAll args <$> (f t >>= go)
+  go (BinaryNoParensType t1 t2 t3) = BinaryNoParensType <$> (f t1 >>= go) <*> (f t2 >>= go) <*> (f t3 >>= go)
+  go (ParensInType t) = ParensInType <$> (f t >>= go)
+  go (ExpandedSynonym t1 t2) = ExpandedSynonym t1 <$> (f t2 >>= go)
+  go other = f other
 
 -- | Replace fully applied type synonyms
 replaceAllTypeSynonyms :: (e ~ MultipleErrors, MonadState CheckState m, MonadError e m) => Type -> m Type
