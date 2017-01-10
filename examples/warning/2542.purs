2b114d73addc4092bbab3b7f7263d731095ccc73
Phil Freeman <paf31@cantab.net>
2017-01-09 19:56:34 -0800

    Comment

--- examples/warning/2542.purs
+++ examples/warning/2542.purs
@@ -5,6 +5,9 @@ import Control.Monad.Eff.Console
 
 type T = forall a. Array a
 
+-- | Note: This should not raise a `ShadowedTypeVar` warning as the 
+-- | type `a` introduced in `T` should not be in scope 
+-- | in the definition of `bar`.
 foo :: T
 foo = bar where
   bar :: forall a. Array a
