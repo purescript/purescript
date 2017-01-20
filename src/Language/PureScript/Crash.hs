93b379a241f091c0af5c66112646e7158f791177
Phil Freeman <paf31@cantab.net>
2017-01-20 09:45:10 -0800

    Fix #2584

--- src/Language/PureScript/Crash.hs
+++ src/Language/PureScript/Crash.hs
@@ -6,6 +6,6 @@ import Prelude.Compat
 internalError :: String -> a
 internalError =
   error
-  . ("An internal error ocurred during compilation: " ++)
+  . ("An internal error occurred during compilation: " ++)
   . (++ "\nPlease report this at https://github.com/purescript/purescript/issues")
   . show
