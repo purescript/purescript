9ad45b9c9594905d31b2def97e881640ba303b24
Gary Burgess <gary.burgess@gmail.com>
2017-02-12 20:50:51 +0000

    LetPattern

--- src/Language/PureScript/Sugar/LetPattern.hs
+++ src/Language/PureScript/Sugar/LetPattern.hs
@@ -18,30 +18,31 @@ desugarLetPatternModule (Module ss coms mn ds exts) = Module ss coms mn (map des
 -- |
 -- Desugar a single let expression
 --
-desugarLetPattern :: Declaration a b -> Declaration a b
+desugarLetPattern :: forall a b. Declaration a b -> Declaration a b
 desugarLetPattern (PositionedDeclaration pos com d ann) = PositionedDeclaration pos com (desugarLetPattern d) ann
 desugarLetPattern decl =
   let (f, _, _) = everywhereOnValues id replace id
   in f decl
   where
   replace :: Expr a b -> Expr a b
-  replace (Let ds e) = go ds e
+  replace (Let ds e ann) = go ann ds e
   replace other = other
 
-  go :: [Declaration a b]
+  go :: b
+     -> [Declaration a b]
      -- ^ Declarations to desugar
      -> Expr a b
      -- ^ The original let-in result expression
      -> Expr a b
-  go [] e = e
-  go (pd@(PositionedDeclaration pos com d) : ds) e =
+  go _ [] e = e
+  go _ (pd@(PositionedDeclaration pos com d ann) : ds) e =
     case d of
-      BoundValueDeclaration {} -> PositionedValue pos com $ go (d:ds) e
-      _ -> append pd $ go ds e
-  go (BoundValueDeclaration binder boundE : ds) e =
-    Case [boundE] [CaseAlternative [binder] [MkUnguarded $ go ds e]]
-  go (d:ds) e = append d $ go ds e
+      BoundValueDeclaration _ _ ann' -> PositionedValue pos com (go ann' (d:ds) e) ann
+      _ -> append ann pd $ go ann ds e
+  go _ (BoundValueDeclaration binder boundE ann : ds) e =
+    Case [boundE] [CaseAlternative [binder] [MkUnguarded $ go ann ds e]] ann
+  go ann (d:ds) e = append ann d $ go ann ds e
 
-  append :: Declaration a b -> Expr a b -> Expr a b
-  append d (Let ds e ann) = Let (d:ds) e ann
-  append d e = Let [d] e
+  append :: b -> Declaration a b -> Expr a b -> Expr a b
+  append _ d (Let ds e ann) = Let (d:ds) e ann
+  append ann d e = Let [d] e ann
