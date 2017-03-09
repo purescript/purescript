0af88caed88709c152ebec3c6251b973bee43407
Phil Freeman <paf31@cantab.net>
2017-03-09 09:50:25 -0800

    TCO Fixes (#2719)
    * Fix #2689 and #2714, updates to TCO
    
    * Tidy up isTailCall a bit
    
    * Remove unused label
    
    * Add JSReturnNoResult statement. General tidying

--- src/Language/PureScript/CodeGen/JS/Optimizer/TCO.hs
+++ src/Language/PureScript/CodeGen/JS/Optimizer/TCO.hs
@@ -4,32 +4,35 @@ module Language.PureScript.CodeGen.JS.Optimizer.TCO (tco) where
 import Prelude.Compat
 
 import Data.Text (Text)
-import Data.Monoid ((<>), getAny, Any(..))
+import Data.Monoid ((<>))
 import Language.PureScript.CodeGen.JS.AST
+import Language.PureScript.AST.SourcePos (SourceSpan)
 
 -- | Eliminate tail calls
 tco :: JS -> JS
 tco = everywhereOnJS convert where
-  tcoLabel :: Text
-  tcoLabel = "tco"
-
   tcoVar :: Text -> Text
   tcoVar arg = "__tco_" <> arg
 
   copyVar :: Text -> Text
   copyVar arg = "__copy_" <> arg
 
+  tcoDone :: Text
+  tcoDone = tcoVar "done"
+
+  tcoLoop :: Text
+  tcoLoop = tcoVar "loop"
+
+  tcoResult :: Text
+  tcoResult = tcoVar "result"
+
   convert :: JS -> JS
-  convert js@(JSVariableIntroduction ss name (Just fn@JSFunction {})) =
-    let
+  convert (JSVariableIntroduction ss name (Just fn@JSFunction {}))
+      | isTailRecursive name body'
+      = JSVariableIntroduction ss name (Just (replace (toLoop name allArgs body')))
+    where
       (argss, body', replace) = collectAllFunctionArgs [] id fn
-    in case () of
-      _ | isTailCall name body' ->
-            let
-              allArgs = concat $ reverse argss
-            in
-              JSVariableIntroduction ss name (Just (replace (toLoop name allArgs body')))
-        | otherwise -> js
+      allArgs = concat $ reverse argss
   convert js = js
 
   collectAllFunctionArgs :: [[Text]] -> (JS -> JS) -> JS -> ([[Text]], JS, JS -> JS)
@@ -43,65 +46,74 @@ tco = everywhereOnJS convert where
     (args : allArgs, body, f . JSReturn s1 . JSFunction s2 ident (map copyVar args))
   collectAllFunctionArgs allArgs f body = (allArgs, body, f)
 
-  isTailCall :: Text -> JS -> Bool
-  isTailCall ident js =
-    let
-      numSelfCalls = everythingOnJS (+) countSelfCalls js
-      numSelfCallsInTailPosition = everythingOnJS (+) countSelfCallsInTailPosition js
-      numSelfCallsUnderFunctions = everythingOnJS (+) countSelfCallsUnderFunctions js
-      numSelfCallWithFnArgs = everythingOnJS (+) countSelfCallsWithFnArgs js
-    in
-      numSelfCalls > 0
-      && numSelfCalls == numSelfCallsInTailPosition
-      && numSelfCallsUnderFunctions == 0
-      && numSelfCallWithFnArgs == 0
-    where
-    countSelfCalls :: JS -> Int
-    countSelfCalls (JSApp _ (JSVar _ ident') _) | ident == ident' = 1
-    countSelfCalls _ = 0
+  isTailRecursive :: Text -> JS -> Bool
+  isTailRecursive ident js = countSelfReferences js > 0 && allInTailPosition js where
+    countSelfReferences = everythingOnJS (+) match where
+      match :: JS -> Int
+      match (JSVar _ ident') | ident == ident' = 1
+      match _ = 0
 
-    countSelfCallsInTailPosition :: JS -> Int
-    countSelfCallsInTailPosition (JSReturn _ ret) | isSelfCall ident ret = 1
-    countSelfCallsInTailPosition _ = 0
-
-    countSelfCallsUnderFunctions :: JS -> Int
-    countSelfCallsUnderFunctions (JSFunction _ _ _ js') = everythingOnJS (+) countSelfCalls js'
-    countSelfCallsUnderFunctions _ = 0
-
-    countSelfCallsWithFnArgs :: JS -> Int
-    countSelfCallsWithFnArgs = go [] where
-      go acc (JSVar _ ident')
-        | ident == ident' && any hasFunction acc = 1
-      go acc (JSApp _ fn args) = go (args ++ acc) fn
-      go _ _ = 0
-
-    hasFunction :: JS -> Bool
-    hasFunction = getAny . everythingOnJS mappend (Any . isFunction)
-      where
-      isFunction JSFunction{} = True
-      isFunction _ = False
+    allInTailPosition (JSReturn _ expr)
+      | isSelfCall ident expr = countSelfReferences expr == 1
+      | otherwise = countSelfReferences expr == 0
+    allInTailPosition (JSWhile _ js1 body)
+      = countSelfReferences js1 == 0 && allInTailPosition body
+    allInTailPosition (JSFor _ _ js1 js2 body)
+      = countSelfReferences js1 == 0 && countSelfReferences js2 == 0 && allInTailPosition body
+    allInTailPosition (JSForIn _ _ js1 body)
+      = countSelfReferences js1 == 0 && allInTailPosition body
+    allInTailPosition (JSIfElse _ js1 body el)
+      = countSelfReferences js1 == 0 && allInTailPosition body && all allInTailPosition el
+    allInTailPosition (JSBlock _ body)
+      = all allInTailPosition body
+    allInTailPosition _
+      = False
 
   toLoop :: Text -> [Text] -> JS -> JS
-  toLoop ident allArgs js = JSBlock rootSS $
+  toLoop ident allArgs js =
+      JSBlock rootSS $
         map (\arg -> JSVariableIntroduction rootSS arg (Just (JSVar rootSS (copyVar arg)))) allArgs ++
-        [ JSLabel rootSS tcoLabel $ JSWhile rootSS (JSBooleanLiteral rootSS True) (JSBlock rootSS [ everywhereOnJS loopify js ]) ]
+        [ JSVariableIntroduction rootSS tcoDone (Just (JSBooleanLiteral rootSS False))
+        , JSVariableIntroduction rootSS tcoResult Nothing
+        ] ++
+        map (\arg ->
+          JSVariableIntroduction rootSS (tcoVar arg) Nothing) allArgs ++
+        [ JSFunction rootSS (Just tcoLoop) allArgs (JSBlock rootSS [loopify js])
+        , JSWhile rootSS (JSUnary rootSS Not (JSVar rootSS tcoDone))
+            (JSBlock rootSS
+              (JSAssignment rootSS (JSVar rootSS tcoResult) (JSApp rootSS (JSVar rootSS tcoLoop) (map (JSVar rootSS) allArgs))
+              : map (\arg ->
+                  JSAssignment rootSS (JSVar rootSS arg) (JSVar rootSS (tcoVar arg))) allArgs))
+        , JSReturn rootSS (JSVar rootSS tcoResult)
+        ]
     where
     rootSS = Nothing
 
     loopify :: JS -> JS
-    loopify (JSReturn ss ret) | isSelfCall ident ret =
-      let
-        allArgumentValues = concat $ collectSelfCallArgs [] ret
-      in
-        JSBlock ss $ zipWith (\val arg ->
-                    JSVariableIntroduction ss (tcoVar arg) (Just val)) allArgumentValues allArgs
-                  ++ map (\arg ->
-                    JSAssignment ss (JSVar ss arg) (JSVar ss (tcoVar arg))) allArgs
-                  ++ [ JSContinue ss tcoLabel ]
+    loopify (JSReturn ss ret)
+      | isSelfCall ident ret =
+        let
+          allArgumentValues = concat $ collectArgs [] ret
+        in
+          JSBlock ss $
+            zipWith (\val arg ->
+              JSAssignment ss (JSVar ss (tcoVar arg)) val) allArgumentValues allArgs
+            ++ [ JSReturnNoResult ss ]
+      | otherwise = JSBlock ss [ markDone ss, JSReturn ss ret ]
+    loopify (JSReturnNoResult ss) = JSBlock ss [ markDone ss, JSReturnNoResult ss ]
+    loopify (JSWhile ss cond body) = JSWhile ss cond (loopify body)
+    loopify (JSFor ss i js1 js2 body) = JSFor ss i js1 js2 (loopify body)
+    loopify (JSForIn ss i js1 body) = JSForIn ss i js1 (loopify body)
+    loopify (JSIfElse ss cond body el) = JSIfElse ss cond (loopify body) (fmap loopify el)
+    loopify (JSBlock ss body) = JSBlock ss (map loopify body)
     loopify other = other
-    collectSelfCallArgs :: [[JS]] -> JS -> [[JS]]
-    collectSelfCallArgs allArgumentValues (JSApp _ fn args') = collectSelfCallArgs (args' : allArgumentValues) fn
-    collectSelfCallArgs allArgumentValues _ = allArgumentValues
+
+    markDone :: Maybe SourceSpan -> JS
+    markDone ss = JSAssignment ss (JSVar ss tcoDone) (JSBooleanLiteral ss True)
+
+    collectArgs :: [[JS]] -> JS -> [[JS]]
+    collectArgs acc (JSApp _ fn args') = collectArgs (args' : acc) fn
+    collectArgs acc _ = acc
 
   isSelfCall :: Text -> JS -> Bool
   isSelfCall ident (JSApp _ (JSVar _ ident') _) = ident == ident'
