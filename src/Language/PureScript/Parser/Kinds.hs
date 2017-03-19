4c0779e40cade988f393c85d228d2c410fe4f134
Gary Burgess <gary.burgess@gmail.com>
2017-03-19 21:46:59 +0000

    Tweak kind syntax hint wording

--- src/Language/PureScript/Parser/Kinds.hs
+++ src/Language/PureScript/Parser/Kinds.hs
@@ -14,11 +14,11 @@ import qualified Text.Parsec.Expr as P
 
 parseStar :: TokenParser Kind
 parseStar = symbol' "*" *>
-  P.parserFail "The `*` symbol is no longer used for type kinds.\n  The new equivalent is the named kind `Type`."
+  P.parserFail "The `*` symbol is no longer used for the kind of types.\n  The new equivalent is the named kind `Type`."
 
 parseBang :: TokenParser Kind
 parseBang = symbol' "!" *>
-  P.parserFail "The `!` symbol is no longer used for effect kinds.\n  The new equivalent is the named kind `Effect`, defined in `Control.Monad.Eff` in the `purescript-eff` library."
+  P.parserFail "The `!` symbol is no longer used for the kind of effects.\n  The new equivalent is the named kind `Effect`, defined in `Control.Monad.Eff` in the `purescript-eff` library."
 
 parseNamedKind :: TokenParser Kind
 parseNamedKind = NamedKind <$> parseQualified kindName
