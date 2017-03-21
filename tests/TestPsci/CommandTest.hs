cb405707ff4ce4343bd066e4453f14b243652f3d
Hyunje Jun <me@noraesae.net>
2017-03-21 12:59:13 +0900

    Implement evaluation test for PSCi (#2747)
    * Add psci-suport to test support deps
    
    * Implement evaluation test for PSCi
    
    Resolve #2664.
    
    * Rename operators in PSCi test environment
    
    Using 'equalsTo' and 'evaluatesTo' is easier to understand.
    
    * Do not evaluate the right part of `evaluatesTo`
    
    The right part just means an expected string output.
    
    * Refactor TestPsci functions
    
    Add comments and rename functions.
    
    * Nudge Travis CI tests

--- tests/TestPsci/CommandTest.hs
+++ tests/TestPsci/CommandTest.hs
@@ -9,23 +9,27 @@ import Test.HUnit
 import TestPsci.TestEnv
 
 commandTests :: Test
-commandTests = TestLabel "commandTests" $ TestList $ map (TestCase . runTestPSCi)
+commandTests = TestLabel "commandTests" $ TestList $ map (TestCase . execTestPSCi)
   [ do
       run "import Prelude"
       run "import Data.Functor"
       run "import Control.Monad"
       before <- psciImportedModules <$> get
-      length before @?== 3
+      length before `equalsTo` 3
       run ":clear"
       after <- psciImportedModules <$> get
-      length after @?== 0
+      length after `equalsTo` 0
   , do
       run "import Prelude"
       run "import Data.Functor"
       run "import Control.Monad"
       before <- psciImportedModules <$> get
-      length before @?== 3
+      length before `equalsTo` 3
       run ":reload"
       after <- psciImportedModules <$> get
-      length after @?== 3
+      length after `equalsTo` 3
+  , do
+      run "import Prelude"
+      run "import Data.Array"
+      "let fac n = foldl mul 1 (1..n) in fac 10" `evaluatesTo` "3628800"
   ]
--- tests/TestPsci/TestEnv.hs
+++ tests/TestPsci/TestEnv.hs
@@ -8,47 +8,74 @@ import           Control.Monad.Trans.RWS.Strict (evalRWST, RWST)
 import qualified Language.PureScript as P
 import           Language.PureScript.Interactive
 import           System.Directory (getCurrentDirectory)
-import           System.Exit (exitFailure)
+import           System.Exit
 import           System.FilePath ((</>))
 import qualified System.FilePath.Glob as Glob
+import           System.Process (readProcessWithExitCode)
 import           Test.HUnit ((@?=))
 
+-- | A monad transformer for handle PSCi actions in tests
 type TestPSCi a = RWST PSCiConfig () PSCiState IO a
 
-initTestPSCi :: IO (PSCiState, PSCiConfig)
-initTestPSCi = do
+-- | Initialise PSCi state and config for tests
+initTestPSCiEnv :: IO (PSCiState, PSCiConfig)
+initTestPSCiEnv = do
+  -- Load test support packages
   cwd <- getCurrentDirectory
   let supportDir = cwd </> "tests" </> "support" </> "bower_components"
   let supportFiles ext = Glob.globDir1 (Glob.compile ("purescript-*/src/**/*." ++ ext)) supportDir
   pursFiles <- supportFiles "purs"
-
-  modulesOrFirstError <- loadAllModules pursFiles
-  case modulesOrFirstError of
+  modulesOrError <- loadAllModules pursFiles
+  case modulesOrError of
     Left err ->
       print err >> exitFailure
     Right modules -> do
-      resultOrErrors <- runMake . make $ modules
-      case resultOrErrors of
+      -- Make modules
+      makeResultOrError <- runMake . make $ modules
+      case makeResultOrError of
         Left errs -> putStrLn (P.prettyPrintMultipleErrors P.defaultPPEOptions errs) >> exitFailure
         Right (externs, env) ->
           return (PSCiState [] [] (zip (map snd modules) externs), PSCiConfig pursFiles env)
 
-runTestPSCi :: TestPSCi a -> IO a
-runTestPSCi i = do
-  (s, c) <- initTestPSCi
+-- | Execute a TestPSCi, returning IO
+execTestPSCi :: TestPSCi a -> IO a
+execTestPSCi i = do
+  (s, c) <- initTestPSCiEnv -- init state and config
   fst <$> evalRWST i c s
 
-testEval :: String -> TestPSCi ()
-testEval = const $ return () -- not yet actually eval expr command
+-- | Evaluate JS to which a PSCi input is compiled. The actual JS input is not
+-- needed as an argument, as it is already written in the file during the
+-- command evaluation.
+jsEval :: TestPSCi String
+jsEval = liftIO $ do
+  writeFile indexFile "require('$PSCI')['$main']();"
+  process <- findNodeProcess
+  result <- traverse (\node -> readProcessWithExitCode node [indexFile] "") process
+  case result of
+    Just (ExitSuccess, out, _)   -> return out
+    Just (ExitFailure _, _, err) -> putStrLn err >> exitFailure
+    Nothing                      -> putStrLn "Couldn't find node.js" >> exitFailure
 
-testReload :: TestPSCi ()
-testReload = return ()
+-- | Run a PSCi command and evaluate the output with 'eval'.
+runAndEval :: String -> TestPSCi () -> TestPSCi ()
+runAndEval comm eval =
+  case parseCommand comm of
+    Left errStr -> liftIO $ putStrLn errStr >> exitFailure
+    Right command ->
+      -- the JS result can be ignored, as it's already written in a source file
+      -- for the detail, please refer to Interactive.hs
+      handleCommand (\_ -> eval) (return ()) command
 
+-- | Run a PSCi command and ignore the output
 run :: String -> TestPSCi ()
-run s = case parseCommand s of
-          Left errStr -> liftIO $ putStrLn errStr >> exitFailure
-          Right command ->
-            handleCommand testEval testReload command
+run comm = runAndEval comm $ jsEval *> return ()
 
-(@?==) :: (Eq a, Show a) => a -> a -> TestPSCi ()
-x @?== y = liftIO $ x @?= y
+-- | A lifted evaluation of HUnit '@?=' for the TestPSCi
+equalsTo :: (Eq a, Show a) => a -> a -> TestPSCi ()
+equalsTo x y = liftIO $ x @?= y
+
+-- | An assertion to check if a command evaluates to a string
+evaluatesTo :: String -> String -> TestPSCi ()
+evaluatesTo command expected = runAndEval command $ do
+  actual <- jsEval
+  actual `equalsTo` (expected ++ "\n")
