3571a5e8e22fe611998b3170cc9ccc2b99bab570
Phil Freeman <paf31@cantab.net>
2017-03-26 14:48:01 -0700

    A couple more TCO tests

--- examples/passing/TCO.purs
+++ examples/passing/TCO.purs
@@ -2,6 +2,8 @@ module Main where
 
 import Prelude
 import Control.Monad.Eff.Console (log, logShow)
+import Control.Monad.Rec.Class
+import Data.Array ((..), span, length)
 
 main = do
   let f x = x + 1
@@ -11,6 +13,12 @@ main = do
   logShow (applyN 2 f v)
   logShow (applyN 3 f v)
   logShow (applyN 4 f v)
+
+  let largeArray = 1..10000
+  logShow (length (span (\_ -> true) largeArray).init)
+
+  logShow (tailRec (\n -> if n < 10000 then Loop (n + 1) else Done 42) 0)
+
   log "Done"
 
 applyN :: forall a. Int -> (a -> a) -> a -> a
