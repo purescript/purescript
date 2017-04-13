f8d4530bfacd339cdd37024b9e7fbe1e8a1412ff
Phil Freeman <paf31@cantab.net>
2017-04-13 09:20:14 -0700

    Allow user to propagate Warn constraints (#2828)
    * Allow user to propagate Warn constraints, fix #2813
    
    * Fix the double warning issue, add an example
    
    * Fix the tests

--- src/Language/PureScript/TypeChecker/Entailment.hs
+++ src/Language/PureScript/TypeChecker/Entailment.hs
@@ -152,8 +152,10 @@ entails SolverOptions{..} constraint context hints =
     solve constraint
   where
     forClassName :: InstanceContext -> Qualified (ProperName 'ClassName) -> [Type] -> [TypeClassDict]
-    forClassName _ C.Warn [msg] =
-      [TypeClassDictionaryInScope (WarnInstance msg) [] C.Warn [msg] Nothing]
+    forClassName ctx cn@C.Warn [msg] =
+      -- Prefer a warning dictionary in scope if there is one available.
+      -- This allows us to defer a warning by propagating the constraint.
+      findDicts ctx cn Nothing ++ [TypeClassDictionaryInScope (WarnInstance msg) [] C.Warn [msg] Nothing]
     forClassName _ C.IsSymbol [TypeLevelString sym] =
       [TypeClassDictionaryInScope (IsSymbolInstance sym) [] C.IsSymbol [TypeLevelString sym] Nothing]
     forClassName _ C.CompareSymbol [arg0@(TypeLevelString lhs), arg1@(TypeLevelString rhs), _] =
