46554e0aa2510c539e5a3e0c8b96282505f1d889
kRITZCREEK <christoph.hegemann1337@gmail.com>
2017-05-29 05:10:59 +0200

    Simplify grouping
    If the groupReexports flag is set, we just treat every export as a
    reexport (Even from the origin module). This will require the editors to do a
    little more work, but makes the behaviours consistent and predictable.

--- src/Language/PureScript/Ide/Completion.hs
+++ src/Language/PureScript/Ide/Completion.hs
@@ -1,6 +1,9 @@
 module Language.PureScript.Ide.Completion
        ( getCompletions
        , getExactMatches
+       , getExactCompletions
+       , simpleExport
+       , completionFromMatch
        , CompletionOptions(..)
        , defaultCompletionOptions
        , applyCompletionOptions
@@ -8,11 +11,12 @@ module Language.PureScript.Ide.Completion
 
 import           Protolude
 
-import           Control.Lens (view)
+import           Control.Lens hiding ((&), op)
 import           Data.Aeson
-import           Data.List (intersect)
 import qualified Data.Map as Map
+import qualified Data.Text as T
 import qualified Language.PureScript as P
+import           Language.PureScript.Ide.Error (prettyPrintTypeSingleLine)
 import           Language.PureScript.Ide.Filter
 import           Language.PureScript.Ide.Matcher
 import           Language.PureScript.Ide.Types
@@ -27,71 +31,111 @@ getCompletions
   -> Matcher IdeDeclarationAnn
   -> CompletionOptions
   -> [Module]
-  -> [Match IdeDeclarationAnn]
+  -> [Completion]
 getCompletions filters matcher options modules =
   modules
   & applyFilters filters
-  & completionsFromModules
+  & matchesFromModules
   & runMatcher matcher
   & applyCompletionOptions options
+  & map completionFromMatch
 
 getExactMatches :: Text -> [Filter] -> [Module] -> [Match IdeDeclarationAnn]
 getExactMatches search filters modules =
-  completionsFromModules (applyFilters (equalityFilter search : filters) modules)
+  modules
+  & applyFilters (equalityFilter search : filters)
+  & matchesFromModules
 
-completionsFromModules :: [Module] -> [Match IdeDeclarationAnn]
-completionsFromModules = foldMap completionFromModule
+getExactCompletions :: Text -> [Filter] -> [Module] -> [Completion]
+getExactCompletions search filters modules =
+  modules
+  & getExactMatches search filters
+  & map simpleExport
+  & map completionFromMatch
+
+matchesFromModules :: [Module] -> [Match IdeDeclarationAnn]
+matchesFromModules = foldMap completionFromModule
   where
     completionFromModule (moduleName, decls) =
       map (\x -> Match (moduleName, x)) decls
 
 data CompletionOptions = CompletionOptions
   { coMaxResults :: Maybe Int
-  , coGroupReexports :: Maybe [P.ModuleName]
+  , coGroupReexports :: Bool
   }
-
-defaultCompletionOptions :: CompletionOptions
-defaultCompletionOptions = CompletionOptions { coMaxResults = Nothing, coGroupReexports = Nothing }
-
-applyCompletionOptions :: CompletionOptions -> [Match IdeDeclarationAnn] -> [Match IdeDeclarationAnn]
-applyCompletionOptions co decls =
-  maybe identity take (coMaxResults co) decls
-  & maybe identity groupCompletionReexports (coGroupReexports co)
-
-groupCompletionReexports :: [P.ModuleName] -> [Match IdeDeclarationAnn] -> [Match IdeDeclarationAnn]
-groupCompletionReexports preferred initial =
-  concatMap choosePreferred (Map.elems (foldr go Map.empty initial))
-  where
-    go (Match (moduleName, d@(IdeDeclarationAnn (view annExportedFrom -> Just origin) decl))) =
-      Map.alter
-      (insertReexport moduleName origin d)
-      (Namespaced (namespaceForDeclaration decl)
-       (P.runModuleName origin <> "." <> identifierFromIdeDeclaration decl))
-    go (Match (moduleName, d@(IdeDeclarationAnn _ decl))) =
-      Map.alter
-      (insertDeclaration moduleName d)
-      (Namespaced (namespaceForDeclaration decl)
-       (P.runModuleName moduleName <> "." <> identifierFromIdeDeclaration decl))
-    insertReexport moduleName origin d old = case old of
-      Nothing -> Just (Match (origin, d), [moduleName])
-      Just x -> Just (second (moduleName :) x)
-    insertDeclaration moduleName d old = case old of
-      Nothing -> Just (Match (moduleName, d), [])
-      Just x -> Just x
-
-    choosePreferred :: (Match a, [P.ModuleName]) -> [Match a]
-    choosePreferred (Match (mn, decl), reexports) =
-      case intersect reexports preferred of
-        [] -> [Match (mn, decl)]
-        prefs -> map (Match . (, decl)) prefs
-
-data Namespaced a = Namespaced IdeNamespace a
-  deriving (Show, Eq, Ord)
 
 instance FromJSON CompletionOptions where
   parseJSON = withObject "CompletionOptions" $ \o -> do
     maxResults <- o .:? "maxResults"
-    groupReexports <- o .:? "groupReexports"
+    groupReexports <- o .:? "groupReexports" .!= False
     pure (CompletionOptions { coMaxResults = maxResults
-                            , coGroupReexports = map P.moduleNameFromString <$> groupReexports
+                            , coGroupReexports = groupReexports
                             })
+
+defaultCompletionOptions :: CompletionOptions
+defaultCompletionOptions = CompletionOptions { coMaxResults = Nothing, coGroupReexports = False }
+
+applyCompletionOptions :: CompletionOptions -> [Match IdeDeclarationAnn] -> [(Match IdeDeclarationAnn, [P.ModuleName])]
+applyCompletionOptions co decls =
+  maybe identity take (coMaxResults co) decls
+  & if coGroupReexports co
+    then groupCompletionReexports
+    else map simpleExport
+
+simpleExport :: Match a -> (Match a, [P.ModuleName])
+simpleExport match@(Match (moduleName, _)) = (match, [moduleName])
+
+groupCompletionReexports :: [Match IdeDeclarationAnn] -> [(Match IdeDeclarationAnn, [P.ModuleName])]
+groupCompletionReexports initial =
+  Map.elems (foldr go Map.empty initial)
+  where
+    go (Match (moduleName, d@(IdeDeclarationAnn ann decl))) =
+      let
+        origin = fromMaybe moduleName (ann^.annExportedFrom)
+      in
+        Map.alter
+        (insertDeclaration moduleName origin d)
+        (Namespaced (namespaceForDeclaration decl)
+         (P.runModuleName origin <> "." <> identifierFromIdeDeclaration decl))
+    insertDeclaration moduleName origin d old = case old of
+      Nothing -> Just ( Match (origin, d & idaAnnotation.annExportedFrom .~ Nothing)
+                      , [moduleName]
+                      )
+      Just x -> Just (second (moduleName :) x)
+
+data Namespaced a = Namespaced IdeNamespace a
+  deriving (Show, Eq, Ord)
+
+completionFromMatch :: (Match IdeDeclarationAnn, [P.ModuleName]) -> Completion
+completionFromMatch (Match (m, IdeDeclarationAnn ann decl), mns) =
+  Completion {..}
+  where
+    (complIdentifier, complExpandedType) = case decl of
+      IdeDeclValue v -> (v ^. ideValueIdent . identT, v ^. ideValueType & prettyPrintTypeSingleLine)
+      IdeDeclType t -> (t ^. ideTypeName . properNameT, t ^. ideTypeKind & P.prettyPrintKind)
+      IdeDeclTypeSynonym s -> (s ^. ideSynonymName . properNameT, s ^. ideSynonymType & prettyPrintTypeSingleLine)
+      IdeDeclDataConstructor d -> (d ^. ideDtorName . properNameT, d ^. ideDtorType & prettyPrintTypeSingleLine)
+      IdeDeclTypeClass d -> (d ^. ideTCName . properNameT, d ^. ideTCKind & P.prettyPrintKind)
+      IdeDeclValueOperator (IdeValueOperator op ref precedence associativity typeP) ->
+        (P.runOpName op, maybe (showFixity precedence associativity (valueOperatorAliasT ref) op) prettyPrintTypeSingleLine typeP)
+      IdeDeclTypeOperator (IdeTypeOperator op ref precedence associativity kind) ->
+        (P.runOpName op, maybe (showFixity precedence associativity (typeOperatorAliasT ref) op) P.prettyPrintKind kind)
+      IdeDeclKind k -> (P.runProperName k, "kind")
+
+    complExportedFrom = mns
+
+    complModule = P.runModuleName m
+
+    complType = maybe complExpandedType prettyPrintTypeSingleLine (_annTypeAnnotation ann)
+
+    complLocation = _annLocation ann
+
+    complDocumentation = Nothing
+
+    showFixity p a r o =
+      let asso = case a of
+            P.Infix -> "infix"
+            P.Infixl -> "infixl"
+            P.Infixr -> "infixr"
+      in T.unwords [asso, show p, r, "as", P.runOpName o]
+
