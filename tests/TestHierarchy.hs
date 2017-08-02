23bc6af5989f2661d8319b82dbc3649dcccb1335
joneshf <jones3.hardy@gmail.com>
2017-08-02 05:48:57 -0700

    Codify more behavior as tests
    We don't build a graph if there are no relations in it.
    Test `prettyPrint` output.

--- tests/TestHierarchy.hs
+++ tests/TestHierarchy.hs
@@ -8,7 +8,34 @@ import Test.Hspec (describe, hspec, it, shouldBe)
 main :: IO ()
 main = hspec $ do
   describe "Language.PureScript.Hierarchy" $ do
+    describe "prettyPrint" $ do
+      it "creates just the node when there is no relation" $ do
+        let superMap = SuperMap (Left $ P.ProperName "A")
+
+        let prettyPrinted = prettyPrint superMap
+
+        prettyPrinted `shouldBe` "  A;"
+
+      it "creates a relation when there is one" $ do
+        let superMap = SuperMap (Right $ (P.ProperName "A", P.ProperName "B"))
+
+        let prettyPrinted = prettyPrint superMap
+
+        prettyPrinted `shouldBe` "  A -> B;"
+
     describe "typeClassGraph" $ do
+      it "doesn't generate a graph if there are no type classes" $ do
+        let mainModule = P.Module
+              (P.internalModuleSourceSpan "<hierarchy>")
+              []
+              (P.ModuleName [P.ProperName "Main"])
+              []
+              Nothing
+
+        let graph = typeClassGraph mainModule
+
+        graph `shouldBe` Nothing
+
       it "generates usable graphviz graphs" $ do
         let declarations =
               [ P.TypeClassDeclaration
