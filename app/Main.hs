d6c6946e88bd0dbcc191b1013df20bff0bc8850a
Marcin Szamotulski <mszamot@gmail.com>
2017-08-19 21:02:21 +0200

    Remove `purs dce` command.

--- app/Main.hs
+++ app/Main.hs
@@ -9,7 +9,6 @@ module Main where
 
 import qualified Command.Bundle as Bundle
 import qualified Command.Compile as Compile
-import qualified Command.DCE as DCE
 import qualified Command.Docs as Docs
 import qualified Command.Hierarchy as Hierarchy
 import qualified Command.Ide as Ide
@@ -54,9 +53,6 @@ main = do
         , Opts.command "compile"
             (Opts.info Compile.command
               (Opts.progDesc "Compile PureScript source files"))
-        , Opts.command "dce"
-            (Opts.info DCE.command
-              (Opts.progDesc "Dead code elimination of CoreFn represantaion"))
         , Opts.command "docs"
             (Opts.info Docs.command
               (Opts.progDesc "Generate Markdown documentation from PureScript source files" <> Docs.infoModList))
