2feac1e1baa67a131276b14d5760aa063d145401
Christoph <christoph.hegemann1337@gmail.com>
2017-11-06 08:25:50 +0100

    remove duplication in benchmark code

--- benchmarks/Ide/Bench.hs
+++ benchmarks/Ide/Bench.hs
@@ -2,18 +2,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Ide.Bench (ideBench) where
 
-import Protolude
+import           Protolude
 
-import System.Directory (getCurrentDirectory, setCurrentDirectory)
-import System.FilePath ((</>))
-import System.Process (createProcess, shell, getProcessExitCode)
-
+import           Criterion.Main (bench, bgroup, Benchmark, nfIO)
 import qualified Criterion.Main as Crit
-import Criterion.Main (bench, bgroup, Benchmark, nfIO)
-
-import Ide.Helper (runIde', defConfig)
+import           Ide.Helper (runIde', defConfig)
 import qualified Language.PureScript.Ide.Command as Command
-import Language.PureScript.Ide.Types
+import           Language.PureScript.Ide.Types
+import           System.Directory (getCurrentDirectory, setCurrentDirectory)
+import           System.FilePath ((</>))
+import           System.Process (createProcess, shell, getProcessExitCode)
 
 inProject :: IO a -> IO a
 inProject f = do
@@ -44,22 +42,21 @@ tryNTimes n action = do
       tryNTimes (n - 1) action
     Just a -> pure (Just a)
 
-loadWithoutGlobs :: IO (ModuleMap [IdeDeclarationAnn])
-loadWithoutGlobs = do
-   (_, st) <- runIde' (defConfig { confGlobs = [] }) emptyIdeState [Command.LoadSync []]
-   pure (vsDeclarations (ideVolatileState st))
-
-loadWithGlobs :: IO (ModuleMap [IdeDeclarationAnn])
-loadWithGlobs = do
+loadWithGlobs :: [FilePath] -> IO (ModuleMap [IdeDeclarationAnn])
+loadWithGlobs globs = do
    (_, st) <-
-     runIde' (defConfig { confGlobs = [ ".psc-package/psc-0.11.6-09272017/*/*/src/**/*.purs" ] })
-     emptyIdeState [Command.LoadSync []]
+     runIde'
+      (defConfig { confGlobs = globs })
+      emptyIdeState
+      [Command.LoadSync []]
    pure (vsDeclarations (ideVolatileState st))
 
 ideBench :: Benchmark
 ideBench =
   Crit.env (inProject compileProject) $ \_ ->
     bgroup "ide"
-      [ bench "Without sourceglobs" $ nfIO (inProject loadWithoutGlobs)
-      , bench "With sourceglobs" $ nfIO (inProject loadWithGlobs)
+      [ bench "Without sourceglobs" $ nfIO $ inProject
+          $ loadWithGlobs []
+      , bench "With sourceglobs" $ nfIO $ inProject
+          $ loadWithGlobs [ ".psc-package/psc-0.11.6-09272017/*/*/src/**/*.purs" ]
       ]
