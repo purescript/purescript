67ab91c1c8cc2cbdfed943bb05de502121234fe8
Matt Parsons <parsonsmatt@gmail.com>
2017-12-15 13:52:09 -0700

    need a new prelude first

--- examples/passing/UInt.purs
+++ examples/passing/UInt.purs
@@ -6,8 +6,8 @@ import Control.Monad.ST
 import Control.Monad.Eff.Console (log, logShow)
 
 main = do
-  let -- uint :: UInt -- TODO: make UInt a type that shows up
+  let uint :: UInt
       uint = 1u
-  -- TODO: actually print the UInt
+  -- TODO: Use the version of Prelude that includes the Show instance
+  -- logShow uint
   log "Done"
-
