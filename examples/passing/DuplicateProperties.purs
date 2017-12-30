ca321c879249b275f18962b9d803d2ef32b5d5e3
Harry Garrood <harry@garrood.me>
2017-12-30 16:28:55 +0000

    Revert "Add proxies (#2846)"
    This reverts commit 74bc4a923dfbd84e1f4dfa448981b57ab3d4ab9c.

--- examples/passing/DuplicateProperties.purs
+++ examples/passing/DuplicateProperties.purs
@@ -3,21 +3,25 @@ module Main where
 import Prelude
 import Control.Monad.Eff.Console (log)
 
-subtractX :: forall r a. @(x :: a | r) -> @r
-subtractX _ = @r
+data RProxy (r :: # Type) = RProxy
 
-extractX :: forall r a. @(x :: a | r) -> @a
-extractX _ = @a
+data Proxy (a :: Type) = Proxy
 
-hasX :: forall r a b. @(x :: a, y :: b | r)
-hasX = @(x :: a, y :: b | r)
+subtractX :: forall r a. RProxy (x :: a | r) -> RProxy r
+subtractX RProxy = RProxy
+
+extractX :: forall r a. RProxy (x :: a | r) -> Proxy a
+extractX RProxy = Proxy
+
+hasX :: forall r a b. RProxy (x :: a, y :: b | r)
+hasX = RProxy
 
 test1 = subtractX (subtractX hasX)
 
 test2
   :: forall r a b
-   . @(x :: a, x :: b, x :: Int | r)
-  -> @Int
+   . RProxy (x :: a, x :: b, x :: Int | r)
+  -> Proxy Int
 test2 x = extractX (subtractX (subtractX x))
 
 main = log "Done"
