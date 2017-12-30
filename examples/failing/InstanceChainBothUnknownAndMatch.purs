517dbb251646b9da9d77ef0ec6c13d416be1058a
Harry Garrood <harry@garrood.me>
2017-12-30 17:08:32 +0000

    Update tests to not use new proxies

--- examples/failing/InstanceChainBothUnknownAndMatch.purs
+++ examples/failing/InstanceChainBothUnknownAndMatch.purs
@@ -1,14 +1,18 @@
 -- @shouldFailWith NoInstanceFound
 module InstanceChains.BothUnknownAndMatch where
 
-class Same l r o | l r -> o
-instance sameY :: Same t t @"Y" else instance sameN :: Same l r @"N"
-same :: forall l r o. Same l r o => l -> r -> @o
-same _ _ = @o
+import Type.Proxy (Proxy)
+import Type.Row (RProxy(..))
+import Data.Symbol (SProxy(..))
+
+class Same l r (o :: Symbol) | l r -> o
+instance sameY :: Same t t "Y" else instance sameN :: Same l r "N"
+same :: forall l r o. Same l r o => l -> r -> SProxy o
+same _ _ = SProxy
 
 -- for label `u`, `t ~ Int` should be Unknown
 -- for label `m`, `Int ~ Int` should be a match
 -- together they should be Unknown
-example :: forall t. @t -> @_
-example _ = same @(u :: t, m :: Int) @(u :: Int, m :: Int)
-
+example :: forall t. Proxy t -> SProxy _
+example _ = same (RProxy :: RProxy (u :: t, m :: Int))
+                 (RProxy :: RProxy (u :: Int, m :: Int))
