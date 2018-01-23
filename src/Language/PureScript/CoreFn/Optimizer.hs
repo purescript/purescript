343e145ef3da38e9ca9744d52d562353b9efdab4
Matthew Leon <ml@matthewleon.com>
2018-01-23 08:21:28 -0500

    optimize inferred partial constraint fn
    fixes https://github.com/purescript/purescript/issues/3157
    
    adds a CoreFn optimization pass

---
+++ src/Language/PureScript/CoreFn/Optimizer.hs
@@ -0,0 +1,29 @@
+module Language.PureScript.CoreFn.Optimizer (optimizeCoreFn) where
+
+import Protolude
+
+import Data.Function (id)
+import Language.PureScript.CoreFn.Expr (Bind(NonRec), Expr(App, Let, Var))
+import Language.PureScript.CoreFn.Module (Module, moduleDecls)
+import Language.PureScript.CoreFn.Traversals (everywhereOnValues)
+import Language.PureScript.Names (Ident(UnusedIdent), Qualified(Qualified))
+
+-- |
+-- CoreFn optimization pass.
+--
+optimizeCoreFn :: Module a -> Module a
+optimizeCoreFn m = m {moduleDecls = optimizeModuleDecls $ moduleDecls m}
+
+optimizeModuleDecls :: [Bind a] -> [Bind a]
+optimizeModuleDecls = map transformBinds
+  where
+  (transformBinds, _, _) = everywhereOnValues id transformExprs id
+
+  transformExprs = optimizeUnusedPartialFn
+
+optimizeUnusedPartialFn :: Expr a -> Expr a
+optimizeUnusedPartialFn (Let _
+  [NonRec _ UnusedIdent _]
+  (App _ (App _ (Var _ (Qualified _ UnusedIdent)) _) originalCoreFn)) =
+  originalCoreFn
+optimizeUnusedPartialFn e = e
