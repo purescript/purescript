1bbaedc907bda39092839139c5a9df0e4a5531a3
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2018-02-09 21:17:42 +0000

    Efficient object updates for closed records
    Closes #1493

--- src/Language/PureScript/CoreFn/Optimizer.hs
+++ src/Language/PureScript/CoreFn/Optimizer.hs
@@ -3,23 +3,29 @@ module Language.PureScript.CoreFn.Optimizer (optimizeCoreFn) where
 import Protolude
 
 import Data.Function (id)
-import Language.PureScript.CoreFn.Expr (Bind(NonRec), Expr(App, Let, Var))
+import Data.List (lookup)
+import Language.PureScript.AST.Literals (Literal(ObjectLiteral))
+import Language.PureScript.AST.SourcePos (nullSourceSpan)
+import Language.PureScript.CoreFn.Ann (Ann)
+import Language.PureScript.CoreFn.Expr (Bind(NonRec), Expr(App, Accessor, Let, ObjectUpdate, Var, Literal))
 import Language.PureScript.CoreFn.Module (Module, moduleDecls)
 import Language.PureScript.CoreFn.Traversals (everywhereOnValues)
-import Language.PureScript.Names (Ident(UnusedIdent), Qualified(Qualified))
+import Language.PureScript.Label (Label(Label))
+import Language.PureScript.Names (ModuleName(ModuleName), ProperName(ProperName), Ident(UnusedIdent), Qualified(Qualified))
+import Language.PureScript.Types (Type(TypeConstructor, RCons, REmpty, TypeApp))
 
 -- |
 -- CoreFn optimization pass.
 --
-optimizeCoreFn :: Module a -> Module a
+optimizeCoreFn :: Module Ann -> Module Ann
 optimizeCoreFn m = m {moduleDecls = optimizeModuleDecls $ moduleDecls m}
 
-optimizeModuleDecls :: [Bind a] -> [Bind a]
+optimizeModuleDecls :: [Bind Ann] -> [Bind Ann]
 optimizeModuleDecls = map transformBinds
   where
   (transformBinds, _, _) = everywhereOnValues id transformExprs id
 
-  transformExprs = optimizeUnusedPartialFn
+  transformExprs = optimizeUnusedPartialFn . optimizeClosedRecordUpdate
 
 -- |
 -- Optimize away function generated to typecheck inferred Partial constraints.
@@ -30,3 +36,24 @@ optimizeUnusedPartialFn (Let _
   (App _ (App _ (Var _ (Qualified _ UnusedIdent)) _) originalCoreFn)) =
   originalCoreFn
 optimizeUnusedPartialFn e = e
+
+optimizeClosedRecordUpdate :: Expr Ann -> Expr Ann
+optimizeClosedRecordUpdate ou@(ObjectUpdate a@(_, _, Just t, _) r updatedFields) =
+  case closedRecordFields t of
+    Nothing -> ou
+    Just allFields -> Literal a (ObjectLiteral (map f allFields))
+      where f (Label l) = case lookup l updatedFields of
+              Nothing -> (l, Accessor (nullSourceSpan, [], Nothing, Nothing) l r)
+              Just e -> (l, e)
+optimizeClosedRecordUpdate e = e
+
+-- | Return the labels of a closed record, or Nothing for other types or open records.
+closedRecordFields :: Type -> Maybe [Label]
+closedRecordFields (TypeApp (TypeConstructor (Qualified (Just (ModuleName [ProperName "Prim"])) (ProperName "Record"))) row) =
+  collect row
+  where
+    collect :: Type -> Maybe [Label]
+    collect REmpty = Just []
+    collect (RCons l _ r) = collect r >>= return . (l :)
+    collect _ = Nothing
+closedRecordFields _ = Nothing
