3916080f84ef3b7717e21f3945e91b8f1ac43667
Christoph <christoph.hegemann1337@gmail.com>
2018-03-19 03:00:12 +0100

    [purs ide] Find usages for recursive definitions
    Previously we'd fail to find these because the name is already in "scope", but
    for recursive definitions that name still refers to the top level declaration.

--- tests/Language/PureScript/Ide/UsageSpec.hs
+++ tests/Language/PureScript/Ide/UsageSpec.hs
@@ -46,6 +46,12 @@ spec = describe "Finding Usages" $ do
                     , usage (Test.mn "FindUsage.Definition") "usageId" IdeNSValue
                     ]
       usage1 `shouldBeUsage` ("src" </> "FindUsage.purs", "12:11-12:18")
+    it "finds a simple recursive usage" $ do
+      ([_, Right (UsagesResult [usage1])], _) <- Test.inProject $
+        Test.runIde [ load ["FindUsage.Recursive"]
+                    , usage (Test.mn "FindUsage.Recursive") "recursiveUsage" IdeNSValue
+                    ]
+      usage1 `shouldBeUsage` ("src" </> "FindUsage" </> "Recursive.purs", "7:12-7:26")
     it "finds a constructor usage" $ do
       ([_, Right (UsagesResult [usage1])], _) <- Test.inProject $
         Test.runIde [ load ["FindUsage", "FindUsage.Definition", "FindUsage.Reexport"]
