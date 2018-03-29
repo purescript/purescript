a71244023375399a8fe21e7d1d1d96f77d3dff67
Tom Harding <tomjharding@live.co.uk>
2018-03-29 21:08:50 +0100

    Allow for constraints in derived functors, quantification shadowing
    - One can now derive functors for types that involve constraints.
    - Shadowed variables are now properly handled in functor deriving.
    
    tl;dr: this works now!
    
    ```
    data T a = T (forall x. Show x => a) (forall a. a)
    ```

--- src/Language/PureScript/Sugar/TypeClasses/Deriving.hs
+++ src/Language/PureScript/Sugar/TypeClasses/Deriving.hs
@@ -742,8 +742,11 @@ deriveFunctor ss mn syns ds tyConNm = do
                     mkAssignment ((Label l), x) = (l, App x (Accessor l argVar))
                 return (lam ss arg (ObjectUpdate argVar (mkAssignment <$> updates)))
 
-          -- quantification
-          goType (ForAll _ t _) = goType t
+          -- quantifiers
+          goType (ForAll scopedVar t _) | scopedVar /= iTyName = goType t
+
+          -- constraints
+          goType (ConstrainedType _ t) = goType t
 
           -- under a `* -> *`, just assume functor for now
           goType (TypeApp _ t) = fmap (App mapVar) <$> goType t
