5f2872f3504bc5c30ab9233700bd88680be46198
joneshf <jones3.hardy@gmail.com>
2018-04-23 08:38:42 -0700

    Tag let bindings with metadata about their origin
    When we parsed where clauses, we desugared directly to a let binding.
    When we did that desugaring,
    we lost the information about whether a let binding was ever a where clause.
    
    We add a bit of metadata to the let bindings we generate
    so we can track whether it began its life as a where clause or not.
    
    This is already showing to be directly useful,
    as we can pretty print where clauses!
    Should allow external tools to also benefit from the metadata.

--- src/Language/PureScript/CoreFn/Desugar.hs
+++ src/Language/PureScript/CoreFn/Desugar.hs
@@ -99,7 +99,7 @@ moduleToCoreFn env (A.Module modSS coms mn decls (Just exps)) =
     Case (ss, com, ty, Nothing) (fmap (exprToCoreFn ss [] Nothing) vs) (fmap (altToCoreFn ss) alts)
   exprToCoreFn ss com _ (A.TypedValue _ v ty) =
     exprToCoreFn ss com (Just ty) v
-  exprToCoreFn ss com ty (A.Let ds v) =
+  exprToCoreFn ss com ty (A.Let w ds v) =
     Let (ss, com, ty, Nothing) (concatMap declToCoreFn ds) (exprToCoreFn ss [] Nothing v)
   exprToCoreFn ss com ty (A.TypeClassDictionaryConstructorApp name (A.TypedValue _ lit@(A.Literal _ (A.ObjectLiteral _)) _)) =
     exprToCoreFn ss com ty (A.TypeClassDictionaryConstructorApp name lit)
