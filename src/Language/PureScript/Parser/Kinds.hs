b8146c880dd4c816a9e99ff2035bee6d6bd1d34f
Liam Goodacre <goodacre.liam@gmail.com>
2018-11-25 00:08:09 +0000

    Remove references to previous kinds * and ! (#3458)
    The kinds * and ! were switched to parse errors in 0.11.
    Now we're at 0.12 we can remove them.

--- src/Language/PureScript/Parser/Kinds.hs
+++ src/Language/PureScript/Parser/Kinds.hs
@@ -12,23 +12,13 @@ import Language.PureScript.Parser.Lexer
 import qualified Text.Parsec as P
 import qualified Text.Parsec.Expr as P
 
-parseStar :: TokenParser Kind
-parseStar = symbol' "*" *>
-  P.parserFail "The `*` symbol is no longer used for the kind of types.\n  The new equivalent is the named kind `Type`."
-
-parseBang :: TokenParser Kind
-parseBang = symbol' "!" *>
-  P.parserFail "The `!` symbol is no longer used for the kind of effects.\n  The new equivalent is the named kind `Effect`, defined in `Control.Monad.Eff` in the `purescript-eff` library."
-
 parseNamedKind :: TokenParser Kind
 parseNamedKind = NamedKind <$> parseQualified kindName
 
 parseKindAtom :: TokenParser Kind
 parseKindAtom =
   indented *> P.choice
-    [ parseStar
-    , parseBang
-    , parseNamedKind
+    [ parseNamedKind
     , parens parseKind
     ]
 
