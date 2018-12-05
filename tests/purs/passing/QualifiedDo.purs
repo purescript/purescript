fa1b5f47bafdc1f03273aa3bdeeb55222cf6be86
Philip Kamenarsky <p.kamenarsky@gmail.com>
2018-12-05 20:02:25 +0300

    Improve QualifiedDo tests

--- tests/purs/passing/QualifiedDo.purs
+++ tests/purs/passing/QualifiedDo.purs
@@ -2,18 +2,18 @@ module Main where
 
 import Prelude
 import Effect.Console (log)
-import IxMonad as I
+import IxMonad as Ix
 
-testIndexed :: forall m a. I.IxMonad m => m a a String
-testIndexed = I.do
-  a <- I.pure "test"
-  b <- I.pure "test"
-  I.pure b
+testIndexed :: forall m a. Ix.IxMonad m => m a a String
+testIndexed = Ix.do
+  a <- Ix.pure "test"
+  b <- Ix.pure "test"
+  Ix.pure (a <> b)
 
 testMonad :: forall m. Monad m => m String
 testMonad = do
   a <- pure "test"
   b <- pure "test"
-  pure b
+  pure (a <> b)
 
 main = log "Done"
