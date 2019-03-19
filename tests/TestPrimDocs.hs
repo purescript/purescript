7e186a29e663d3664046450410bb288882970783
Harry Garrood <harry@garrood.me>
2019-03-19 00:39:40 +0000

    Convert prim docs tests to use tasty (#3568)

--- tests/TestPrimDocs.hs
+++ tests/TestPrimDocs.hs
@@ -2,38 +2,40 @@ module TestPrimDocs where
 
 import Prelude
 
-import Control.Monad
-import Data.List ((\\))
+import Data.List (sort)
+import Control.Exception (evaluate)
+import Control.DeepSeq (force)
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import qualified Language.PureScript as P
 import qualified Language.PureScript.Docs as D
-import qualified Language.PureScript.Docs.AsMarkdown as D
 
-main :: IO ()
-main = do
-  putStrLn "Test that there are no bottoms hiding in primDocsModule"
-  seq (D.runDocs (D.modulesAsMarkdown D.primModules)) (return ())
+import Test.Tasty
+import Test.Tasty.Hspec (Spec, testSpec, it)
+import Test.Hspec (shouldBe)
 
-  putStrLn "Test that Prim is fully documented"
-  let actualPrimNames =
-        -- note that prim type classes are listed in P.primTypes
-        (map (P.runProperName . P.disqualify . fst) $ Map.toList
-          ( P.primTypes <>
-            P.primBooleanTypes <>
-            P.primOrderingTypes <>
-            P.primRowTypes <>
-            P.primRowListTypes <>
-            P.primTypeErrorTypes <>
-            P.primSymbolTypes )) ++
-        (map (P.runProperName . P.disqualify) $ Set.toList P.allPrimKinds)
-  let documentedPrimNames = map D.declTitle (concatMap D.modDeclarations D.primModules)
+main :: IO TestTree
+main = testSpec "prim docs" spec
 
-  let undocumentedNames = actualPrimNames \\ documentedPrimNames
-  let extraNames = documentedPrimNames \\ actualPrimNames
+spec :: Spec
+spec = do
+  it "there are no bottoms hiding in primModules" $ do
+    _ <- evaluate (force D.primModules)
+    return ()
 
-  when (not (null undocumentedNames)) $
-    error $ "Undocumented Prim names: " ++ show undocumentedNames
+  it "all Prim modules are fully documented" $ do
+    let actualPrimNames =
+          -- note that prim type classes are listed in P.primTypes
+          (map (P.runProperName . P.disqualify . fst) $ Map.toList
+            ( P.primTypes <>
+              P.primBooleanTypes <>
+              P.primOrderingTypes <>
+              P.primRowTypes <>
+              P.primRowListTypes <>
+              P.primTypeErrorTypes <>
+              P.primSymbolTypes )) ++
+          (map (P.runProperName . P.disqualify) $ Set.toList P.allPrimKinds)
+    let documentedPrimNames =
+          map D.declTitle (concatMap D.modDeclarations D.primModules)
 
-  when (not (null extraNames)) $
-    error $ "Extra Prim names: " ++ show extraNames
+    sort documentedPrimNames `shouldBe` sort actualPrimNames
