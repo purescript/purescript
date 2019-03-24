99213368c9a7fd4b2862dec5c3ec1214c8801743
Harry Garrood <harry@garrood.me>
2019-03-24 14:41:20 +0000

    Add test for resolved dependencies with no version
    Previously, purs publish would fail when given any dependencies which
    did not resolve to a version (#3061). This bug was fixed by #3565. This
    commit just adds a test to verify that the bug has been fixed.

--- tests/purs/publish/basic-example/src/Main.purs
+++ tests/purs/publish/basic-example/src/Main.purs
@@ -3,6 +3,14 @@ module Main where
 import Prelude
 import Effect (Effect)
 import Effect.Console (log)
+import Data.Newtype (class Newtype, un)
+
+newtype Target = Target String
+
+derive instance newtypeTarget :: Newtype Target _
+
+greetingTarget :: Target
+greetingTarget = Target "world"
 
 main :: Effect Unit
-main = log ("hello, " <> "world!")
+main = log ("hello, " <> un Target greetingTarget <> "!")
