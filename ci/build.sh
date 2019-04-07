5f9ac285e9ef7c5b0b2116a2c9469f8244a36437
Harry Garrood <harry@garrood.me>
2019-04-07 21:19:39 +0100

    Use one build script for all CI builds (#3587)
    This should allow us to get appveyor working again. Since we upgraded to
    GHC 8.6.4, all appyeyor builds seem to have failed because we need to
    recompile all of the compiler's Haskell dependencies, which takes too
    long. Using the existing Travis script on AppVeyor too means that we can
    reuse the timeout mechanism to allow us to pick up where we left off
    if builds do time out.

--- ci/build.sh
+++ ci/build.sh
@@ -1,12 +1,49 @@
 #!/bin/bash
 set -e
 
+# This is the main CI build script. It is intended to run on all platforms we
+# run CI on: linux, mac os, and windows (via msys). It makes use of the
+# following environment variables:
+#
+# BUILD_TYPE
+# Must be one of the following:
+#  - "normal": Compile & run tests normally
+#  - "sdist": Create a source distribution and check that everything still
+#    compiles and works
+#  - "haddock": Check that haddock documentation builds correctly.
+#
+# CI_RELEASE
+# If set to "true", passes the RELEASE flag to the compiler and enables
+# optimizations.
+
 STACK="stack --no-terminal --jobs=1"
 [[ "$BUILD_TYPE" == "haddock" ]] && DEPS_HADDOCK="--haddock"
 
+# This command is ludicrously verbose on Windows, so we pipe the output to a
+# file and only display it if the command fails.
+if ! $STACK --verbosity=error setup 1>stack-setup.log 2>&1
+then
+  cat stack-setup.log
+  echo "Failed to run 'stack setup'"
+  exit 1
+fi
+
 # Setup & install dependencies or abort
 ret=0
-$TIMEOUT 40m $STACK --install-ghc build \
+if [ -x "C:\\msys64\\usr\\bin\\timeout.exe" ]
+then
+  TIMEOUT=C:\\msys64\\usr\\bin\\timeout.exe
+elif which timeout >/dev/null
+then
+  TIMEOUT=timeout
+elif which gtimeout >/dev/null
+then
+  TIMEOUT=gtimeout
+else
+  echo "timeout command not found (nor gtimeout)"
+  exit 1
+fi
+$TIMEOUT 40m $STACK build \
   --only-dependencies --test $DEPS_HADDOCK \
   || ret=$?
 case "$ret" in
@@ -25,13 +62,13 @@ esac
 
 # Set up configuration
 STACK_EXTRA_FLAGS=""
-if [ -z "$TRAVIS_TAG" ]
+if [ "$CI_RELEASE" = "true" ]
 then
-  # On non-release builds, disable optimizations.
-  STACK_EXTRA_FLAGS="--fast"
-else
   # On release builds, set the 'release' cabal flag.
   STACK_EXTRA_FLAGS="--flag purescript:RELEASE"
+else
+  # On non-release builds, disable optimizations.
+  STACK_EXTRA_FLAGS="--fast"
 fi
 
 if [ "$STACKAGE_NIGHTLY" = "true" ]
