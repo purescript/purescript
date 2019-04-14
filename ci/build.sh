78608ea695de9ce46a873ef65055b5f48c989dac
Harry Garrood <harry@garrood.me>
2019-04-14 12:52:45 +0100

    Use travis_wait for timeouts

--- ci/build.sh
+++ ci/build.sh
@@ -11,40 +11,6 @@ set -ex
 #   If set to "true", passes the RELEASE flag to the compiler, and enables
 #   optimizations. Otherwise, we disable optimizations (to speed builds up).
 # 
-# = Stack & GHC
-# 
-# The script expects `stack setup` to have been run already; if this script is
-# run and the correct version GHC is not available, we can end up having stack
-# attempt to install all of the compiler's dependencies before the script
-# runs. (This is problematic because then the installation won't be subject to
-# the timeout mechanism inside the script).
-# 
-# = Timeouts
-# 
-# Unfortunately, we are forced to implement a command timeout mechanism for
-# this script. At the time of writing:
-# 
-# - Both CI platforms we use limit the length of time builds may take. Travis
-#   CI's limit is 50 minutes, and AppVeyor's is one hour.
-# - Setting up GHC and installing all of the compiler's dependencies takes
-#   longer than this limit allows.
-# 
-# Both Travis CI and AppVeyor provide a build cache mechanism, which allows you
-# to cache compiled artifacts in order to speed subsequent builds up. However,
-# when builds time out, we don't get the opportunity to upload a cache (since
-# we've already run out of time). Therefore, if we want the progress we have
-# made in a build to be saved to the build cache, we need to make sure we abort
-# the build early to allow time to upload the cache. Then, the next commit can
-# pick up where the previous commit left off.
-# 
-# == What to do when a build times out
-# 
-# If a CI build times out, you need to push a new commit. Amending and
-# force-pushing DOES NOT WORK. I suspect this is because CI platforms will only
-# consider a particular build cache to be appropriate to use when building a
-# given commit with if the cache was created by a parent of the commit being
-# built (which is sensible of them).
-# 
 # = Source distributions
 # 
 # During a normal build, we create a source distribution with `stack sdist`,
@@ -64,43 +30,13 @@ STACK="stack --no-terminal --jobs=1"
 STACK_OPTS="--test --haddock"
 if [ "$CI_RELEASE" = "true" ]
 then
-  STACK_OPTS="$STACK_OPTS --fast"
-else
   STACK_OPTS="$STACK_OPTS --flag=purescript:RELEASE"
-fi
-
-# Setup & install dependencies or abort
-ret=0
-if [ -x "/c/msys64/usr/bin/timeout.exe" ]
-then
-  TIMEOUT=/c/msys64/usr/bin/timeout.exe
-elif which timeout >/dev/null
-then
-  TIMEOUT=timeout
-elif which gtimeout >/dev/null
-then
-  TIMEOUT=gtimeout
 else
-  echo "timeout command not found (nor gtimeout)"
-  exit 1
+  STACK_OPTS="$STACK_OPTS --fast"
 fi
 
-$TIMEOUT 35m \
-  $STACK build --only-dependencies $STACK_OPTS \
-  || ret=$?
-case "$ret" in
-  0) # continue
-    ;;
-  124)
-    echo "Timed out while installing dependencies."
-    echo "Try pushing a new commit to build again."
-    exit 1
-    ;;
-  *)
-    echo "Failed to install dependencies."
-    exit 1
-    ;;
-esac
+# Install dependencies
+$STACK build --only-dependencies $STACK_OPTS
 
 # Test in a source distribution (see above)
 $STACK sdist --tar-dir sdist-test;
