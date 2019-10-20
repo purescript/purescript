24084aa217ac5f6039806b3c24d2eb0cc6cf4356
Harry Garrood <harry@garrood.me>
2019-10-20 19:55:23 +0100

    Test for local variable shadowing

--- tests/purs/bundle/ObjectShorthand.purs
+++ tests/purs/bundle/ObjectShorthand.purs
@@ -4,11 +4,15 @@ module Main (main) where
 import Prelude
 import Effect (Effect)
 import Effect.Console (log)
-import Test.Assert (assert)
+import Test.Assert (assert')
 
 main :: Effect Unit
 main = do
-  assert (bar.foo == "foo")
+  assert' "bar" (bar.foo == 1)
+  assert' "quux" (quux 3 == { baz: 3 })
+  assert' "baz" bazIsEliminated
   log "Done"
 
-foreign import bar :: { foo :: String }
+foreign import bar :: { foo :: Int }
+foreign import quux :: forall a. a -> { baz :: a }
+foreign import bazIsEliminated :: Boolean
