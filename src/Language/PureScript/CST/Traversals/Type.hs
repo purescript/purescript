f875c2828a68682ed9a681d14fe87166b7d948a5
Nathan Faubion <nathan@n-son.com>
2020-02-01 07:35:58 -0800

    Fix cst traversal for types

--- src/Language/PureScript/CST/Traversals/Type.hs
+++ src/Language/PureScript/CST/Traversals/Type.hs
@@ -17,7 +17,7 @@ everythingOnTypes op k = goTy
     TypeRow _ (Wrapped _ row _) -> goRow ty row
     TypeRecord _ (Wrapped _ row _) -> goRow ty row
     TypeForall _ _ _ _ ty2 -> k ty `op` goTy ty2
-    TypeKinded _ ty2 _ _ -> k ty `op` goTy ty2
+    TypeKinded _ ty2 _ ty3 -> k ty `op` (goTy ty2 `op` goTy ty3)
     TypeApp _ ty2 ty3 -> k ty `op` (goTy ty2 `op` goTy ty3)
     TypeOp _ ty2 _ ty3 -> k ty `op` (goTy ty2 `op` goTy ty3)
     TypeOpName _ _ -> k ty
