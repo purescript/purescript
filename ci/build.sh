b37c1f853717e4aaef8100c45fdac572913d9173
joneshf <jones3.hardy@gmail.com>
2020-03-05 21:11:25 -0800

    Setup sdists for each package
    Now that we have more than one package, we have to do things a little
    different. `stack sdist` will create an sdist for each package. This
    means we end up with a directory that looks like:
    
    ```console
    $ tree sdist-test
    sdist-test
    ├── purescript-0.13.6.tar.gz
    ├── purescript-ast-0.1.0.0.tar.gz
    └── purescript-cst-0.1.0.0.tar.gz
    
    0 directories, 3 files
    ```
    
    If we try to use `tar -xzf sdist-test/purescript-*.tar.gz -C sdist-test
    --strip-components=1`, the glob will expand to all three sdists and fail
    with a cryptic error:
    
    ```console
    $ tar -xzf sdist-test/purescript-0.13.6.tar.gz sdist-test/purescript-ast-0.1.0.0.tar.gz sdist-test/purescript-cst-0.1.0.0.tar.gz -C sdist-test --strip-components=1
    tar: sdist-test/purescript-ast-0.1.0.0.tar.gz: Not found in archive
    tar: sdist-test/purescript-cst-0.1.0.0.tar.gz: Not found in archive
    tar: Exiting with failure status due to previous errors
    ```
    
    We change to mirroring the directory structure within the `sdist-test`
    directory, and running that way.
    
    It's a bit unfortunate that the top-level package has to be handled
    differently, but this should help us move forward.

--- ci/build.sh
+++ ci/build.sh
@@ -34,7 +34,11 @@ fi
 $STACK build --only-snapshot $STACK_OPTS
 
 # Test in a source distribution (see above)
-$STACK sdist --tar-dir sdist-test;
+$STACK sdist lib/purescript-ast --tar-dir sdist-test/lib/purescript-ast
+tar -xzf sdist-test/lib/purescript-ast/purescript-ast-*.tar.gz -C sdist-test/lib/purescript-ast --strip-components=1
+$STACK sdist lib/purescript-cst --tar-dir sdist-test/lib/purescript-cst
+tar -xzf sdist-test/lib/purescript-cst/purescript-cst-*.tar.gz -C sdist-test/lib/purescript-cst --strip-components=1
+$STACK sdist . --tar-dir sdist-test;
 tar -xzf sdist-test/purescript-*.tar.gz -C sdist-test --strip-components=1
 pushd sdist-test
 $STACK build --pedantic $STACK_OPTS
