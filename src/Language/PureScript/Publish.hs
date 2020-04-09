ab30f326c554b8916ff917010fc4f2a08d681b15
Gleb Popov <6yearold@gmail.com>
2020-04-09 22:29:34 +0400

    Relax some version bounds and fix build with Cabal 3.

--- src/Language/PureScript/Publish.hs
+++ src/Language/PureScript/Publish.hs
@@ -19,7 +19,7 @@ module Language.PureScript.Publish
   , getModules
   ) where
 
-import Protolude hiding (stdin)
+import Protolude hiding (stdin, lines)
 
 import Control.Arrow ((***))
 import Control.Category ((>>>))
@@ -36,7 +36,7 @@ import Data.Time.Clock (UTCTime)
 import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
 import Data.Version
 import qualified Distribution.SPDX as SPDX
-import qualified Distribution.Parsec.Class as CabalParsec
+import qualified Distribution.Parsec as CabalParsec
 
 import System.Directory (doesFileExist)
 import System.FilePath.Glob (globDir1)
