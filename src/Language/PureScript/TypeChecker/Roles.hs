fe57d49715f23bcc4eeae942173fd004381778e9
Cyril <sobierajewicz.cyril@gmail.com>
2020-06-04 23:51:37 +0200

    Recurse on the right of arrows and under foralls when inferring nominal roles from kinds (#3896)
    * Recurse on the right of arrows when inferring nominal roles from kinds
    
    * Recurse under foralls when inferring nominal roles from kinds
    
    * Infer nominal roles from kinds arity

--- src/Language/PureScript/TypeChecker/Roles.hs
+++ src/Language/PureScript/TypeChecker/Roles.hs
@@ -139,11 +139,11 @@ inferRoles env tyName
 -- the absence of a role signature, provides the safest default for a type whose
 -- constructors are opaque to us.
 rolesFromForeignTypeKind :: SourceType -> [Role]
-rolesFromForeignTypeKind
-  = go []
-  where
-    go acc = \case
-      TypeApp _ (TypeApp _ fn k1) _k2 | eqType fn tyFunction ->
-        go (Nominal : acc) k1
-      _k ->
-        Nominal : acc
+rolesFromForeignTypeKind k = replicate (kindArity k) Nominal
+
+kindArity :: SourceType -> Int
+kindArity = go 0 where
+  go n (TypeApp _ (TypeApp _ fn _) k)
+    | fn == tyFunction = go (n + 1) k
+  go n (ForAll _ _ _ k _) = go n k
+  go n _ = n
