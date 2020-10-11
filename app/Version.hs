4c77eb7bafb0dfa4d38457890f107af13e76a8b2
Harry Garrood <harry@garrood.me>
2020-10-11 17:16:04 +0100

    Update version to 0.14.0-rc3

--- app/Version.hs
+++ app/Version.hs
@@ -17,7 +17,7 @@ import qualified Development.GitRev as GitRev
 -- prerelease identifier here (if any). When releasing a proper version, simply
 -- set this to an empty string.
 prerelease :: String
-prerelease = "-rc2"
+prerelease = "-rc3"
 
 versionString :: String
 versionString = showVersion Paths.version ++ prerelease ++ extra
