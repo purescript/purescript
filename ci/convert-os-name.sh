396b0861319baca202bb97c418248e5fd30a2111
Jordan Martinez <jordanalex.martinez@gmail.com>
2021-01-03 18:15:16 -0800

    Add initial release.yml file

--- ci/convert-os-name.sh
+++ ci/convert-os-name.sh
@@ -3,14 +3,20 @@
 # binary bundles.
 set -e
 
-case "$TRAVIS_OS_NAME" in
+case "$1" in
   "linux")
+    echo linux64;;
+  "Linux")
     echo linux64;;
   "osx")
     echo macos;;
+  "macOS")
+    echo macos;;
   "windows")
     echo win64;;
+  "Windows")
+    echo win64;;
   *)
-    echo "Unknown TRAVIS_OS_NAME: $TRAVIS_OS_NAME";
+    echo "Unknown OS Name: $1";
     exit 1;;
 esac
