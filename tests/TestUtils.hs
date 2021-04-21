741b9c0bb36d062857126280484f876ce2625d83
Ryan Hendrickson <ryan.hendrickson@alum.mit.edu>
2021-04-21 15:18:13 -0400

    Remove setup-win.cmd

--- tests/TestUtils.hs
+++ tests/TestUtils.hs
@@ -26,7 +26,6 @@ import Data.Time.Clock (UTCTime())
 import Data.Tuple (swap)
 import System.Process hiding (cwd)
 import System.Directory
-import System.Info
 import System.IO.UTF8 (readUTF8FileT)
 import System.Exit (exitFailure)
 import System.FilePath
@@ -51,15 +50,12 @@ findNodeProcess = runMaybeT . msum $ map (MaybeT . findExecutable) names
 updateSupportCode :: IO ()
 updateSupportCode = do
   setCurrentDirectory "tests/support"
-  if System.Info.os == "mingw32"
-    then callProcess "setup-win.cmd" []
-    else do
-      callProcess "npm" ["install"]
-      -- bower uses shebang "/usr/bin/env node", but we might have nodejs
-      node <- maybe cannotFindNode pure =<< findNodeProcess
-      -- Sometimes we run as a root (e.g. in simple docker containers)
-      -- And we are non-interactive: https://github.com/bower/bower/issues/1162
-      callProcess node ["node_modules/.bin/bower", "--allow-root", "install", "--config.interactive=false"]
+  callCommand "npm install"
+  -- bower uses shebang "/usr/bin/env node", but we might have nodejs
+  node <- maybe cannotFindNode pure =<< findNodeProcess
+  -- Sometimes we run as a root (e.g. in simple docker containers)
+  -- And we are non-interactive: https://github.com/bower/bower/issues/1162
+  callProcess node ["node_modules/bower/bin/bower", "--allow-root", "install", "--config.interactive=false"]
   setCurrentDirectory "../.."
   where
   cannotFindNode :: IO a
