602bac7d2db595aef345098e45419cf8d518f2d4
Ryan Hendrickson <ryan.hendrickson@alum.mit.edu>
2021-04-22 13:45:42 -0400

    Migrate CI from Travis to GitHub Actions

--- ci/build.sh
+++ ci/build.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+# Provides expanders that group console output in GitHub Actions
+# See https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#grouping-log-lines
+(echo "::group::Initialize variables") 2>/dev/null
+
 # This is the main CI build script. It is intended to run on all platforms we
 # run CI on: linux, mac os, and windows. It makes use of the following
 # environment variables:
@@ -35,9 +39,13 @@ fi
 # with an invalid option error.
 export TASTY_NO_CREATE=true
 
+(echo "::endgroup::"; echo "::group::Install snapshot dependencies") 2>/dev/null
+
 # Install snapshot dependencies (since these will be cached globally and thus
 # can be reused during the sdist build step)
 $STACK build --only-snapshot $STACK_OPTS
+
+(echo "::endgroup::"; echo "::group::Build source distributions") 2>/dev/null
 
 # Test in a source distribution (see above)
 $STACK sdist lib/purescript-ast --tar-dir sdist-test/lib/purescript-ast
@@ -46,6 +54,11 @@ $STACK sdist lib/purescript-cst --tar-dir sdist-test/lib/purescript-cst
 tar -xzf sdist-test/lib/purescript-cst/purescript-cst-*.tar.gz -C sdist-test/lib/purescript-cst --strip-components=1
 $STACK sdist . --tar-dir sdist-test;
 tar -xzf sdist-test/purescript-*.tar.gz -C sdist-test --strip-components=1
+
+(echo "::endgroup::"; echo "::group::Build and test PureScript") 2>/dev/null
+
 pushd sdist-test
 $STACK build $STACK_OPTS
 popd
+
+(echo "::endgroup::") 2>/dev/null
