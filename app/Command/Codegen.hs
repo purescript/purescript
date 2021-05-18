47467b913bcf6a91e17cd0434954b31c276493dd
Colin Wahl <colin.t.wahl@gmail.com>
2021-05-18 14:53:57 -0700

    WIP - run codegen

--- app/Command/Codegen.hs
+++ app/Command/Codegen.hs
@@ -4,8 +4,23 @@ import Prelude
 
 import           Control.Applicative (many)
 import           Control.Monad (when, unless)
-import qualified Language.PureScript as P
+import           Control.Monad.IO.Class (MonadIO(..))
+import           Control.Monad.Supply
+
+import qualified Data.Aeson.Internal as A
+import           Data.Aeson.Parser (eitherDecodeWith, json)
+import qualified Data.ByteString.Lazy as BSL
+import           Data.Either (rights)
+import qualified Data.Set as S
+import qualified Data.Map as M
+
 import qualified Options.Applicative as Opts
+
+import qualified Language.PureScript.Docs.Types as Docs
+import qualified Language.PureScript as P
+import qualified Language.PureScript.CoreFn as CoreFn
+import qualified Language.PureScript.CoreFn.FromJSON as CoreFn
+
 import           System.Exit (exitFailure, exitSuccess)
 import           System.FilePath.Glob (glob)
 import           System.IO (hPutStr, hPutStrLn, stderr)
@@ -17,16 +32,54 @@ data CodegenOptions = CodegenOptions
 
 codegen :: CodegenOptions -> IO ()
 codegen CodegenOptions{..} = do
-  input <- globWarningOnMisses (unless codegenJSONErrors . warnFileTypeNotFound) codegenCoreFnInput
-  when (null input && not codegenJSONErrors) $ do
+  inputFiles <- globWarningOnMisses (unless codegenJSONErrors . warnFileTypeNotFound) codegenCoreFnInput
+  when (null inputFiles && not codegenJSONErrors) $ do
     hPutStr stderr $ unlines
       [ "purs codegen: No input files."
       , "Usage: For basic information, try the `--help` option."
       ]
     exitFailure
+  mods <- traverse parseCoreFn inputFiles
+  let
+    filePathMap =
+      M.fromList $ map (\m -> (CoreFn.moduleName m, Right $ CoreFn.modulePath m)) $ map snd $ rights mods
 
+  foreigns <- P.inferForeignModules filePathMap
+  _ <-
+    liftIO
+      $ P.runMake purescriptOptions
+      $ runSupplyT 0
+      $ traverse (runCodegen foreigns filePathMap) $ map snd $ rights mods
   exitSuccess
   where
+  parseCoreFn file = do
+    contents <- BSL.readFile file
+    case eitherDecodeWith json (A.iparse CoreFn.moduleFromJSON) contents of
+      Left (j, e) -> pure $ Left (file, j, e)
+      Right r -> pure $ Right r
+
+  makeActions foreigns filePathMap = P.buildMakeActions "output" filePathMap foreigns False
+
+  purescriptOptions :: P.Options
+  purescriptOptions = P.Options False False (S.fromList [ P.JS ])
+
+  runCodegen foreigns filePathMap m =
+    P.codegen (makeActions foreigns filePathMap) m
+      (Docs.Module (CoreFn.moduleName m) Nothing [] [])
+      (moduleToExternsFile m)
+
+  moduleToExternsFile :: CoreFn.Module a -> P.ExternsFile
+  moduleToExternsFile CoreFn.Module {CoreFn.moduleName} = P.ExternsFile {
+      P.efVersion      = mempty,
+      P.efModuleName   = moduleName,
+      P.efExports      = [],
+      P.efImports      = [],
+      P.efFixities     = [],
+      P.efTypeFixities = [],
+      P.efDeclarations = [],
+      P.efSourceSpan   = P.SourceSpan "none" (P.SourcePos 0 0) (P.SourcePos 0 0)
+    }
+
   warnFileTypeNotFound :: String -> IO ()
   warnFileTypeNotFound =
     hPutStrLn stderr . ("purs graph: No files found using pattern: " <>)
