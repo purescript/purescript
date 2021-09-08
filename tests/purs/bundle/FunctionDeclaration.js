1e376c74c718bbf66316137359fa6389f4424c6d
Berk Ozkutuk <ozkutuk.berk@gmail.com>
2021-09-09 00:50:28 +0300

    Add top-level function call to bundler test

--- tests/purs/bundle/FunctionDeclaration.js
+++ tests/purs/bundle/FunctionDeclaration.js
@@ -17,3 +17,9 @@ exports.qux = qux;
 var fs = require('fs');
 var source = fs.readFileSync(__filename, 'utf-8');
 exports.fooIsEliminated = !/^ *var foo/m.test(source);
+
+function localFunction() {
+  return true;
+}
+
+localFunction(); // this should cause localFunction to be bundled
