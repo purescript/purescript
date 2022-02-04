9d997ea6a8fa4db32f7b800c9a48c55419b3f796
sigma-andex <sigma.andex@pm.me>
2022-02-04 20:07:33 +0000

    Fix tests

--- tests/purs/bundle/ObjectShorthand.js
+++ tests/purs/bundle/ObjectShorthand.js
@@ -1,4 +1,4 @@
-var foo = 1;
+export var foo = 1;
 
 export var bar = { foo };
 
--- tests/purs/optimize/2866.out.js
+++ tests/purs/optimize/2866.out.js
@@ -2,14 +2,13 @@
 // Canonical test for #2866. This doesn't need to test whether `apply`s
 // defined from modules other than `Data.Function` are incorrectly
 // optimized since the rest of the test suite seemingly catches it.
-"use strict";
 var Area = function (x) {
     return x;
 };
 var areaFlipped = 42;
 var area = 42;
-module.exports = {
-    Area: Area,
-    area: area,
-    areaFlipped: areaFlipped
+export {
+    Area,
+    area,
+    areaFlipped
 };
