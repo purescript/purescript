6e4c8be8954d37f8b7675cd18a4ecd9a04288d93
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-02-22 18:06:23 -0600

    Add test for ffi constraint workaround

---
+++ tests/purs/passing/FFIConstraintWorkaround.purs
@@ -0,0 +1,19 @@
+module FFIConstraintWorkaround where
+
+import Prelude
+import Effect
+import Effect.Console
+
+main :: Effect Unit
+main = do
+  log $ showFFI 4
+  log $ showFFI "string"
+  log $ showFFI { a: 1, b: true, c: 'd', e: 4.0 }
+
+showFFI :: forall a. Show a => a -> String
+showFFI = showImpl show
+
+-- Since type class constraints are not allowed
+-- in FFI declarations, we have to pass members
+-- we want to use into the function itself.
+foreign import showImpl :: forall a. (a -> String) -> a -> String
