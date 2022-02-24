acaca5a561231e94c18e91a56ed0970580a2f443
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-02-23 19:56:55 -0600

    Assert that strings match expected content

--- tests/purs/passing/FFIConstraintWorkaround.purs
+++ tests/purs/passing/FFIConstraintWorkaround.purs
@@ -3,9 +3,14 @@ module Main where
 import Prelude
 import Effect
 import Effect.Console
+import Test.Assert
 
 main :: Effect Unit
 main = do
+  assert' "Showing Int is correct" $ showFFI 4 == "4"
+  assert' "Showing String is correct" $ showFFI "string" == "\"string\""
+  assert' "Showing Record is correct" $
+    showFFI { a: 1, b: true, c: 'd', e: 4.0 } == "{ a: 1, b: true, c: 'd', e: 4.0 }"
   log "Done"
 
 showFFI :: forall a. Show a => a -> String
