010a2d613a9bd3c0f3cc387a2d2bc36ce0842517
JordanMartinez <jordanalex.martinez@protonmail.com>
2022-02-26 19:57:10 -0600

    Disable constraints in FFI (#4240)
    * Error when constraints are found in FFI values
    
    For example...
    ```
    foreign import foo :: forall a. Show a => a -> String
    ```
    
    Constraints are not allowed because one can implicitly rely upon
    the dictionary's representation at runtime.
    
    * Add test for FFI constraint workaround
    
    In other words, type class dictionary members (e.g. `show` below)
    must be passed explicitly into the FFI function for one to use
    them in FFI.
    ```
    foreign import fooImpl :: forall a. (a -> String) -> a -> String
    
    foo :: forall a. Show a => a -> String
    foo a = fooImpl show a
    ```
    
    Note: this test may break if we change the Show instances
    for any of the used types in `purescript-prelude`.
    
    * Rename test files to drop 'Deprecated'
    
    * Add changelog entry

--- tests/purs/passing/FunWithFunDeps.js
+++ tests/purs/passing/FunWithFunDeps.js
@@ -9,22 +9,18 @@ export var fcons = function (hd) {
   };
 };
 
-export var fappend = function (dict) {
-  return function (left) {
-    return function (right) {
-      return left.concat(right);
-    };
+export var fappendImpl = function (left) {
+  return function (right) {
+    return left.concat(right);
   };
 };
 
-export var fflatten = function (dict) {
-  return function (v) {
-    var accRef = [];
-    for (var indexRef = 0; indexRef < v.length; indexRef += 1) {
-      accRef = accRef.concat(v[indexRef]);
-    }
-    return accRef;
-  };
+export var fflattenImpl = function (v) {
+  var accRef = [];
+  for (var indexRef = 0; indexRef < v.length; indexRef += 1) {
+    accRef = accRef.concat(v[indexRef]);
+  }
+  return accRef;
 };
 
 export var ftoArray = function (vect) {
