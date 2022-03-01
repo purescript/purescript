6590563396dc952a92148e44a326caa43a2f8d76
JordanMartinez <jordanalex.martinez@protonmail.com>
2022-03-01 16:34:38 -0600

    Make first alpha release for v0.15.0 (#4251)
    * Make first alpha release for v0.15.0
    
    We wanted to use `0.15.0-alpha.1`, but cabal
    couldn't parse the `purescript.cabal` file.
    
    So, we're going with `-alpha-01` prefix
    in case we go over 10 alpha releases

--- app/Version.hs
+++ app/Version.hs
@@ -17,7 +17,7 @@ import qualified Development.GitRev as GitRev
 -- prerelease identifier here (if any). When releasing a proper version, simply
 -- set this to an empty string.
 prerelease :: String
-prerelease = ""
+prerelease = "-alpha-01"
 
 versionString :: String
 versionString = showVersion Paths.version ++ prerelease ++ extra
