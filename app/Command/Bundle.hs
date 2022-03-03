10c0d4d2ccee5f6c77c7e4a97309320c2346e2c3
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-03 13:48:11 -0600

    `purs bundle` prints link to migration guide

--- app/Command/Bundle.hs
+++ app/Command/Bundle.hs
@@ -9,7 +9,11 @@ import qualified Options.Applicative as Opts
 
 app :: IO ()
 app = do
-  hPutStrLn stderr "'purs bundle' was removed in the v0.15.0 release. Use 'esbuild' or another bundler."
+  hPutStrLn stderr $ unlines
+    [ "'purs bundle' was removed in the v0.15.0 release."
+    , "Use 'esbuild' or another bundler. For info on migrating to a bundler, see"
+    , "https://github.com/purescript/documentation/blob/master/migration-guides/0.15-Migration-Guide.md#how-can-i-bundle-my-library-or-application"
+    ]
   exitFailure
 
 -- | Make it go.
