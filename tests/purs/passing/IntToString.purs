ba2f26c2c9d17c5d5389806e552827f11ad40daf
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-14 18:53:58 -0700

    Add Prim.Int.ToString compiler-solved class

---
+++ tests/purs/passing/IntToString.purs
@@ -0,0 +1,26 @@
+module Main where
+
+import Prelude
+import Effect.Console (log)
+import Prim.Int (class ToString)
+
+data Proxy :: forall k. k -> Type
+data Proxy a = Proxy
+
+type One = 1
+type NegOne = (-1)
+type Zero = 0
+
+testToString :: forall i s. ToString i s => Proxy i -> Proxy s
+testToString _ = Proxy
+
+posToString :: Proxy "1"
+posToString = testToString (Proxy :: Proxy 1)
+
+negToString :: Proxy "-1"
+negToString = testToString (Proxy :: Proxy (-1))
+
+zeroToString :: Proxy "0"
+zeroToString = testToString (Proxy :: Proxy 0)
+
+main = log "Done"
