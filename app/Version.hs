b742c05dc443dacc0e0f573db565689d2e2410ae
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-04-15 15:49:54 -0500

    Make alpha-05 release

--- app/Version.hs
+++ app/Version.hs
@@ -17,7 +17,7 @@ import qualified Development.GitRev as GitRev
 -- prerelease identifier here (if any). When releasing a proper version, simply
 -- set this to an empty string.
 prerelease :: String
-prerelease = "-alpha-04"
+prerelease = "-alpha-05"
 
 versionString :: String
 versionString = showVersion Paths.version ++ prerelease ++ extra
