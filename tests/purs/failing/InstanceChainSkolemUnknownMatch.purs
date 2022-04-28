8ae7ba397526d5e3999cb825adad8c9c8e22fd94
JordanMartinez <jordanalex.martinez@protonmail.com>
2022-04-28 13:09:38 -0500

    Update tests and their bower dependencies to 0.15.0 library versions (#4300)
    * Update test's bower.json files to released versions
    
    * Update passing tests
    
    * Fix duplicate module error
    
    prelude defines `Data.Reflectable` module now
    
    * Update golden tests
    
    * Install spago 0.20.8
    
    * Add changelog entry

--- tests/purs/failing/InstanceChainSkolemUnknownMatch.purs
+++ tests/purs/failing/InstanceChainSkolemUnknownMatch.purs
@@ -2,14 +2,13 @@
 module InstanceChainSkolemUnknownMatch where
 
 import Type.Proxy (Proxy(..))
-import Data.Symbol (SProxy(..))
 
 class Same l r (o :: Symbol) | l r -> o
 instance sameY :: Same t t "Y" else instance sameN :: Same l r "N"
-same :: forall l r o. Same l r o => l -> r -> SProxy o
-same _ _ = SProxy
+same :: forall l r o. Same l r o => l -> r -> Proxy o
+same _ _ = Proxy
 
 -- shouldn't discard sameY as Apart
-example :: forall (t :: Type). Proxy t -> SProxy _
+example :: forall (t :: Type). Proxy t -> Proxy _
 example _ = same (Proxy :: Proxy t) (Proxy :: Proxy Int)
 
