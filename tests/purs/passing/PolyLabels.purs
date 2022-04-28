2fbfc0c1d2585bbc9b68bbcc17637cf7db94ea9c
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-04-28 11:27:22 -0500

    Update passing tests

--- tests/purs/passing/PolyLabels.purs
+++ tests/purs/passing/PolyLabels.purs
@@ -4,7 +4,8 @@ import Prelude
 import Prim.Row
 import Effect
 import Effect.Console
-import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
+import Data.Reflectable (class Reflectable, reflectType)
+import Type.Proxy (Proxy(..))
 
 foreign import unsafeGet
   :: forall r a
@@ -21,44 +22,44 @@ foreign import unsafeSet
 
 get
   :: forall r r' l a
-   . IsSymbol l
+   . Reflectable l String
   => Cons l a r' r
-  => SProxy l
+  => Proxy l
   -> Record r
   -> a
-get l = unsafeGet (reflectSymbol l)
+get l = unsafeGet (reflectType l)
 
 set
   :: forall r1 r2 r l a b
-   . IsSymbol l
+   . Reflectable l String
   => Cons l a r r1
   => Cons l b r r2
-  => SProxy l
+  => Proxy l
   -> b
   -> Record r1
   -> Record r2
-set l = unsafeSet (reflectSymbol l)
+set l = unsafeSet (reflectType l)
 
 lens 
   :: forall l f r1 r2 r a b
-   . IsSymbol l
+   . Reflectable l String
   => Cons l a r r1
   => Cons l b r r2
   => Functor f
-  => SProxy l
+  => Proxy l
   -> (a -> f b)
   -> Record r1
   -> f (Record r2)
 lens l f r = flip (set l) r <$> f (get l r)
 
 getFoo :: forall a r. { foo :: a | r } -> a
-getFoo = get (SProxy :: SProxy "foo")
+getFoo = get (Proxy :: Proxy "foo")
 
 setFoo :: forall a b r. b -> { foo :: a | r } -> { foo :: b | r }
-setFoo = set (SProxy :: SProxy "foo")
+setFoo = set (Proxy :: Proxy "foo")
 
 fooLens :: forall f a b r. Functor f => (a -> f b) -> { foo :: a | r } -> f { foo :: b | r } 
-fooLens = lens (SProxy :: SProxy "foo")
+fooLens = lens (Proxy :: Proxy "foo")
 
 main :: Effect Unit
 main = do
