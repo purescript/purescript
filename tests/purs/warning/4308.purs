e529919ac569ea7fe7c3490ad93feb33bcedc446
Fyodor Soikin <name.fa@gmail.com>
2022-05-06 14:39:40 -0400

    Comment

--- tests/purs/warning/4308.purs
+++ tests/purs/warning/4308.purs
@@ -7,8 +7,8 @@ module Main where
 f :: Int
 f = (\(y :: _) -> (y :: _)) 42
 
--- All three warnings expected here because `g` has a wildcard in it: one
--- warning for the top-level signature wildcard, one in the lambda parameter
--- pattern, and one in the lambda body.
+-- All three warnings expected here because the type signature of `g` has a
+-- wildcard in it. One warning for the top-level signature wildcard, one in the
+-- lambda parameter pattern, and one in the lambda body.
 g :: _
 g = (\(y :: _) -> (y :: _)) 42
