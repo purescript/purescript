7e4a34ed048b9c6a4f178d676d67c988f8e24652
Harry Garrood <harry@garrood.me>
2022-05-15 20:14:52 +0100

    Update to GHC 9.2.2

--- src/Language/PureScript/TypeChecker/Entailment.hs
+++ src/Language/PureScript/TypeChecker/Entailment.hs
@@ -264,15 +264,19 @@ entails SolverOptions{..} constraint context hints =
                            sortOn (tcdChain &&& tcdIndex)
                            dicts
                   -- process instances in a chain in index order
-                  let found = for (tails1 chain) $ \(tcd :| tl) ->
-                                -- Make sure the type unifies with the type in the type instance definition
-                                case matches typeClassDependencies tcd tys'' of
-                                  Apart        -> Right ()                   -- keep searching
-                                  Match substs -> Left (Right (substs, tcd)) -- found a match
-                                  Unknown ->
-                                    if null (tcdChain tcd) || null tl
-                                    then Right ()                                   -- need proof of apartness but this is either not in a chain or at the end
-                                    else Left (Left (tcdToInstanceDescription tcd)) -- can't continue with this chain yet, need proof of apartness
+                  let found = for (init $ tails chain) $ \tail' ->
+                                case tail' of
+                                  (tcd:tl) ->
+                                    -- Make sure the type unifies with the type in the type instance definition
+                                    case matches typeClassDependencies tcd tys'' of
+                                      Apart        -> Right ()                   -- keep searching
+                                      Match substs -> Left (Right (substs, tcd)) -- found a match
+                                      Unknown ->
+                                        if null (tcdChain tcd) || null tl
+                                        then Right ()                                   -- need proof of apartness but this is either not in a chain or at the end
+                                        else Left (Left (tcdToInstanceDescription tcd)) -- can't continue with this chain yet, need proof of apartness
+                                  [] ->
+                                    internalError "solve: tail' is empty"
 
                   lefts [found]
             solution <- lift . lift $ unique kinds'' tys'' ambiguous instances (unknownsInAllCoveringSets tys'' typeClassCoveringSets)
