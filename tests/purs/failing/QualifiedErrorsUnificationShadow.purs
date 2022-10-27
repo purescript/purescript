3d05987b1792b8a4ed8bf49d5c1423c5a124a49c
Verity Scheel <monoidmusician@gmail.com>
2022-10-26 21:30:44 -0500

    Quantified shadowing test

---
+++ tests/purs/failing/QualifiedErrorsUnificationShadow.purs
@@ -0,0 +1,9 @@
+-- @shouldFailWith TypesDoNotUnify
+module Main where
+
+import Prelude (Unit)
+import Data.List.Lazy (nil)
+import Data.List.Types (List) as Data.List.Lazy.Types
+
+wrongModule :: Data.List.Lazy.Types.List Unit
+wrongModule = nil
