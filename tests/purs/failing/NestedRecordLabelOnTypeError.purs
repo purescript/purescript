58593721f4f3220489aed9fc746a959d16562e7b
Edvard Thörnros <edvard.thornros@gmail.com>
2022-11-02 09:10:24 +0100

    Update the erroneous source file

--- tests/purs/failing/NestedRecordLabelOnTypeError.purs
+++ tests/purs/failing/NestedRecordLabelOnTypeError.purs
@@ -2,7 +2,7 @@
 module NestedRecordLabelOnTypeError where
 
 record :: { a :: { b :: { c :: Int } } }
-record = { field: 1 }
+record = { a: { b: { c: 1 } } }
 
 error :: { a :: { b :: { c :: String } } }
 error = record -- this should trigger an error, telling us there's a mismatch in the field `a > b > c`
