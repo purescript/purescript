56bfddeedb1317cf9c8740002486c8136a1a93fd
Edvard Thörnros <edvard.thornros@gmail.com>
2022-11-12 21:11:27 +0100

    Fix typo and update comment

--- tests/purs/failing/RecordLabelOnTypeErrorImediate.purs
+++
@@ -1,5 +0,0 @@
--- @shouldFailWith TypesDoNotUnify
-module NestedRecordLabelOnTypeError where
-
-record :: { a :: Int }
-record = { a: "a" } -- this should trigger an error, telling us there's a mismatch in the field `a`, but it doesn't
