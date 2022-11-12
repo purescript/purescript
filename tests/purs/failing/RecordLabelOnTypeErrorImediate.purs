24542d46462e2941e2e44dd6dc93e40aba73e0df
Edvard Thörnros <edvard.thornros@gmail.com>
2022-11-12 16:09:32 +0100

    Revert "Update comppiler tests"
    This reverts commit 4b67f95e3f3ada483d405da0c490b04ead6a5160.

--- tests/purs/failing/RecordLabelOnTypeErrorImediate.purs
+++ tests/purs/failing/RecordLabelOnTypeErrorImediate.purs
@@ -1,5 +1,5 @@
 -- @shouldFailWith TypesDoNotUnify
 module NestedRecordLabelOnTypeError where
 
-record :: { field :: Int }
-record = { field: "a" } -- this should trigger an error, telling us there's a mismatch in the field `a`.
+record :: { a :: Int }
+record = { a: "a" } -- this should trigger an error, telling us there's a mismatch in the field `a`, but it doesn't
