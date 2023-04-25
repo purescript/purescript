6cd482a9a330a688e5a82b3cc963ccb8f3b620be
Justin Garcia <purefunctor@gmail.com>
2023-04-26 02:32:44 +0800

    Fix exhaustiveness checking to account for case guards

---
+++ tests/purs/failing/4466.purs
@@ -0,0 +1,17 @@
+-- @shouldFailWith NoInstanceFound
+module Main where
+
+import Prelude
+
+import Data.Array as Array
+import Data.Maybe (Maybe(..))
+import Partial.Unsafe (unsafePartial)
+
+data Sound = Moo | Quack | Bark
+
+type Animal = { sound :: Sound }
+
+animalFunc :: Array Animal -> Unit
+animalFunc animals
+  | Just { sound } <- animals # Array.find \{ sound: Moo } -> true = unit
+  | otherwise = unit
