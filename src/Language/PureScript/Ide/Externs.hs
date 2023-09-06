62d066101156528948145ddd40758ca1b2f9ece1
Jonatan Borkowski <jonatan@thebo.me>
2023-09-06 07:47:58 +0000

    Remove Show

--- src/Language/PureScript/Ide/Externs.hs
+++ src/Language/PureScript/Ide/Externs.hs
@@ -10,7 +10,6 @@ import Protolude hiding (to, from, (&))
 import Codec.CBOR.Term as Term
 import Control.Lens (preview, view, (&), (^.))
 import "monad-logger" Control.Monad.Logger (MonadLogger, logErrorN)
-import Data.Version (showVersion)
 import Data.Text qualified as Text
 import Language.PureScript qualified as P
 import Language.PureScript.Make.Monad qualified as Make
@@ -40,7 +39,7 @@ readExternFile fp = do
         _ ->
           throwError (GeneralError ("Parsing the extern at: " <> toS fp <> " failed"))
     where
-      version = toS (showVersion P.currentVersion)
+      version = toS P.currentVersion
 
 convertExterns :: P.ExternsFile -> ([IdeDeclarationAnn], [(P.ModuleName, P.DeclarationRef)])
 convertExterns ef =
