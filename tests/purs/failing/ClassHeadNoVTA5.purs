151a7a958c556053f134754f1dca86612cbf2387
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-11-01 08:54:12 -0500

    Fix comment

--- tests/purs/failing/ClassHeadNoVTA5.purs
+++ tests/purs/failing/ClassHeadNoVTA5.purs
@@ -1,6 +1,8 @@
 -- @shouldFailWith NoInstanceFound
 module Main where
 
+-- Verify that args in output match order defined here:
+-- `tyNotAppearInBody` appears before `norThisOne`
 class MultiFdBidi tyNotAppearInBody norThisOne | tyNotAppearInBody -> norThisOne, norThisOne -> tyNotAppearInBody where
   useMultiFdBidi :: Int
 
--- tests/purs/failing/ClassHeadNoVTA6.purs
+++ tests/purs/failing/ClassHeadNoVTA6.purs
@@ -1,8 +1,6 @@
 -- @shouldFailWith NoInstanceFound
 module Main where
 
--- Verify that the order of fun deps declared here
--- match the order shown in output
 class Multi a b c d e f | f e -> a b c d, a b -> c d e f where
   useMulti :: Int
 
