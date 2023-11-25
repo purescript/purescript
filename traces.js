0ff77c170deaa335fe5ce0ac5fcf80fd99c1f289
Verity Scheel <monoidmusician@gmail.com>
2023-11-24 20:27:02 -0600

    Concurrency stats for traces.js

--- traces.js
+++ traces.js
@@ -4,75 +4,111 @@
 // Process it with
 //     eventlog2html --json purs.eventlog
 //     node trace.js purs.eventlog.json
-var filename = process.argv[2];
-if (!filename) throw new Error("Provide a file name");
-var eventlog = JSON.parse(require("fs").readFileSync(filename, "utf-8"));
+var mainFile = process.argv[2];
+if (!mainFile) throw new Error("Provide a file name");
 
-var traces = {};
 var name_length = 0;
+
+function summarizeEventlog(filename) {
+    var eventlog = JSON.parse(require("fs").readFileSync(filename, "utf-8"));
+    // eventlog.heap
+    //   c: Set(3) { 'Heap Size', 'Live Bytes', 'Blocks Size' }
+    // eventlog.samples
+    // eventlog.traces
+
+    var traces = {};
+    var minTx = Infinity;
+    var maxTx = -Infinity;
+    var total = 0;
+    var con = [];
+    var max_cons = [[]];
+    var cursor = 0;
+
+    // I guess some buffering makes it out of order?
+    eventlog.traces.sort(({tx: tx1}, {tx: tx2}) => tx1 - tx2);
+
+    for (let trace of eventlog.traces) {
+        var m = /^([\w.]+) (start|end)$/.exec(trace.desc);
+        if (!m) continue;
+        var name = m[1];
+        if (!(name in traces)) traces[name] = {};
+        if (name.length > name_length) name_length = name.length;
+        var ev = m[2];
+
+        if (traces[name][ev]) {
+            if (traces[name].time === 0) {
+                console.log("Warn: start after end", name, traces[name].start, trace.tx);
+            } else {
+                console.log("Warn: duplicate event", trace.desc);
+            }
+            continue;
+        }
+
+        var time = trace.tx;
+        if (time < minTx) minTx = time;
+        if (time > maxTx) maxTx = time;
+
+        while (cursor < eventlog.heap.length && eventlog.heap[cursor].x < trace.tx) {
+            cursor++;
+            if (eventlog.heap[cursor].c !== 'Heap Size') {
+                cursor = eventlog.heap.length;
+            }
+        }
+        if (ev === "start") {
+            traces[name].cursor = cursor;
+        }
+
+        traces[name][ev] = time;
+        if (ev === "end" && !("start" in traces[name])) {
+            console.log("Warn: missing start for", name);
+            traces[name].start = time;
+            traces[name].time = 0;
+            continue;
+        }
+        if ("start" in traces[name] && "end" in traces[name]) {
+            traces[name].time = traces[name].end - traces[name].start;
+            var mems = eventlog.heap.slice(traces[name].cursor, cursor).map(e => e.y);
+            var mem_min = Math.min(...mems);
+            var mem_max = Math.max(...mems);
+            Object.assign(traces[name], {mem_min,mem_max});
+            total += traces[name].time;
+        }
+
+        if (ev === "start") con = con.concat([name]);
+        if (ev === "end") {
+            var l = con.length;
+            con = con.filter(n => n !== name);
+            if (con.length !== l - 1) {
+                console.log(con, name);
+            }
+        }
+        if (con.length >= max_cons[0].length) {
+            if (con.length > max_cons[0].length)
+                max_cons = [];
+            max_cons.push(con);
+        }
+    }
+
+    var timespan = maxTx - minTx;
+
+    return { traces, total, minTx, maxTx, timespan, max_cons };
+}
+
+var { traces, total, timespan, max_cons } = summarizeEventlog(mainFile);
+
 var timings = [];
-var minTx = Infinity;
-var maxTx = -Infinity;
-var total = 0;
-var con = [];
-var max_cons = [[]];
-
-// I guess some buffering makes it out of order?
-eventlog.traces.sort(({tx: tx1}, {tx: tx2}) => tx1 - tx2);
-
-for (let trace of eventlog.traces) {
-    var m = /^([\w.]+) (start|end)$/.exec(trace.desc);
-    if (!m) continue;
-    var name = m[1];
-    if (!(name in traces)) traces[name] = {};
-    if (name.length > name_length) name_length = name.length;
-    var ev = m[2];
-    if (traces[name][ev]) {
-        if (traces[name].time === 0) {
-            console.log("Warn: start after end", name, traces[name].start, trace.tx);
-        } else {
-            console.log("Warn: duplicate event", trace.desc);
-        }
-        continue;
-    }
-    traces[name][ev] = trace.tx;
-    if (ev === "end" && !("start" in traces[name])) {
-        console.log("Warn: missing start for", name);
-        traces[name].start = trace.tx;
-        traces[name].time = 0;
-        continue;
-    }
-    if (trace.tx < minTx) minTx = trace.tx;
-    if (trace.tx > maxTx) maxTx = trace.tx;
-    if ("start" in traces[name] && "end" in traces[name]) {
-        traces[name].time = traces[name].end - traces[name].start;
-        timings.push([name, traces[name].time]);
-        total += traces[name].time;
-    }
-
-    if (ev === "start") con = con.concat([name]);
-    if (ev === "end") {
-        var l = con.length;
-        con = con.filter(n => n !== name);
-        if (con.length !== l - 1) {
-            console.log(con, name);
-        }
-    }
-    if (con.length >= max_cons[0].length) {
-        if (con.length > max_cons[0].length)
-            max_cons = [];
-        max_cons.push(con);
-    }
-}
 for (let name in traces) {
-    if (!("time" in traces[name]))
-        console.log("Warn: missing timing for", name, traces[name]);
+    let trace = traces[name];
+    if (!("time" in trace)) {
+        console.log("Warn: missing timing for", name, trace);
+    } else if (trace.time > 0) {
+        timings.push([name, trace.time]);
+    }
 }
 
-var timespan = maxTx - minTx;
+timings.sort(([n1,t1,_1,m1], [n2,t2,_2,m2]) => t1 - t2);
 
-timings.sort(([n1,t1], [n2,t2]) => t1 - t2);
-timings.push(["stats", "----"]);
+timings.push(["stats", "-----"]);
 timings.push(["total", total]);
 timings.push(["timespan", timespan]);
 timings.push(["ratio (avg concurrency?)", total/timespan]);
@@ -97,6 +133,7 @@ timings.push(["time at max concurrency (s)", max_con_time]);
 timings.push(["time at max concurrency (%)", 100*max_con_time/timespan]);
 
 for (let [name, time] of timings) {
+    // console.log(name.padEnd(name_length, " "), (""+time).substring(0, 5).padStart(5, " "));
     console.log(name.padEnd(name_length, " "), time);
 }
 
