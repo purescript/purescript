ba05707f9b6902b4678e5b914dace9331d05f0a6
Justin Garcia <purefunctor@gmail.com>
2024-02-26 11:04:40 +0800

    Add test cases

---
+++ tests/purs/passing/4535.purs
@@ -0,0 +1,38 @@
+module Main where
+
+import Prelude
+
+import Data.Maybe (Maybe(..))
+import Data.Tuple.Nested ((/\), type (/\))
+import Effect (Effect)
+import Effect.Console (log)
+
+singleArgument :: forall @a. a -> Unit
+singleArgument _ = unit
+
+multiArgument :: forall @a @b. a -> b -> Unit
+multiArgument _ _ = unit
+
+singleApplication :: Int /\ Number -> Unit
+singleApplication = singleArgument @(Int /\ Number)
+
+-- Like expression applications, visible type applications are left-associative.
+-- This test accounts for subsequent type applications nested in this manner.
+appNestingWorks :: Int /\ Number -> Number /\ Int -> Unit
+appNestingWorks = multiArgument @(Int /\ Number) @(Number /\ Int)
+
+-- This test accounts for type applications nested within other AST nodes.
+otherNestingWorks :: Array (Maybe (Int /\ Number))
+otherNestingWorks = [Just @(Int /\ Number) (0 /\ 0.0), Just @(Int /\ Number) (1 /\ 1.0)]
+
+type InSynonym = Int /\ Number
+
+-- This test accounts for type synonyms used as type arguments.
+-- Since expansion happens during checking, InSynonym would expand
+-- to an already-desugared type operator. This test exists for the
+-- sake of redundancy.
+inSynonym :: InSynonym -> Unit
+inSynonym = singleArgument @InSynonym
+
+main :: Effect Unit
+main = log "Done"
