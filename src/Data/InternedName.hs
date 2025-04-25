ff1d6b53f1e33af0ddcec89fcc86c6ce0f7fc67d
seastian <seastian@users.noreply.github.com>
2025-04-25 10:49:42 +0200

    interned name

---
+++ src/Data/InternedName.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE DerivingStrategies #-}
+module Data.InternedName where
+
+import Prelude
+import Data.Aeson (FromJSON(..), ToJSON(..))
+import Data.Interned (intern, unintern)
+import Data.Interned.Text (InternedText)
+import Control.DeepSeq (NFData(..))
+import Codec.Serialise (Serialise(..), encode, decode)
+import Data.Text as T
+
+newtype InternedName = InternedName InternedText
+  deriving stock (Show)
+  deriving newtype (Eq, Ord)
+
+internName :: String -> InternedName
+internName name = InternedName $ intern $ T.pack name
+
+uninternName :: InternedName -> String
+uninternName (InternedName name) = T.unpack $ unintern name
+
+instance FromJSON InternedName where
+  parseJSON = fmap internName . parseJSON
+
+instance ToJSON InternedName where
+  toJSON = toJSON . uninternName
+
+instance Serialise InternedName where
+  encode = encode . uninternName
+  decode = fmap internName decode
+
+instance NFData InternedName where
+  rnf (InternedName name) = ()
--- src/Language/PureScript/AST/Declarations/ChainId.hs
+++ src/Language/PureScript/AST/Declarations/ChainId.hs
@@ -5,42 +5,18 @@ module Language.PureScript.AST.Declarations.ChainId
 
 import Prelude
 import Language.PureScript.AST.SourcePos qualified as Pos
+import Data.InternedName (InternedName, internName)
 import Control.DeepSeq (NFData)
 import Codec.Serialise (Serialise)
-import Data.Aeson (ToJSON, FromJSON (parseJSON), withObject, (.:), object, (.=))
-import Data.Interned (intern, unintern)
-import Data.Interned.String (InternedString)
-import Data.Aeson.Types (ToJSON(..))
-import Protolude (NFData(rnf))
-import Codec.Serialise.Class (Serialise(encode, decode))
+import Data.Aeson (ToJSON, FromJSON)
 
 -- |
 -- For a given instance chain, stores the chain's file name and
 -- the starting source pos of the first instance in the chain.
 -- This data is used to determine which instances are part of
 -- the same instance chain.
-newtype ChainId = ChainId (InternedString, Pos.SourcePos)
-  deriving (Eq, Ord, Show)
-
-instance Serialise ChainId where
-  encode (ChainId (str, pos)) = encode (unintern str, pos)
-  decode = do
-    (str, pos) <- decode
-    pure $ ChainId (intern str, pos)
-
-instance NFData ChainId where
-  rnf (ChainId (internedStr, pos)) =
-    rnf (unintern internedStr) `seq` rnf pos
-
-instance FromJSON ChainId where
-  parseJSON = withObject "ChainId" $ \v -> do
-    str <- v .: "string"
-    pos <- v .: "sourcePos"
-    return $ ChainId (intern str, pos)
-
-instance ToJSON ChainId where
-  toJSON (ChainId (internedStr, pos)) =
-    object ["string" .= unintern internedStr, "sourcePos" .= pos]
+newtype ChainId = ChainId (InternedName, Pos.SourcePos)
+  deriving (Eq, Ord, Show, NFData, Serialise, ToJSON, FromJSON)
 
 mkChainId :: String -> Pos.SourcePos -> ChainId
-mkChainId fileName startingSourcePos = ChainId (intern fileName, startingSourcePos)
+mkChainId fileName startingSourcePos = ChainId (internName fileName, startingSourcePos)
