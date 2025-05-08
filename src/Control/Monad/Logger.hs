2f017add16b857b524e24b235db64833b9dbe60f
Michal Kozakiewicz <morswin@gmail.com>
2025-05-08 21:10:45 +0000

    Specialize logger to errors

--- src/Control/Monad/Logger.hs
+++ src/Control/Monad/Logger.hs
@@ -12,37 +12,38 @@ import Control.Monad.Trans.Control (MonadBaseControl(..))
 import Control.Monad.Writer.Class (MonadWriter(..))
 
 import Data.IORef (IORef, atomicModifyIORef', newIORef, readIORef)
+import Language.PureScript.Errors (MultipleErrors (MultipleErrors))
 
 -- | Logger monad, using IORef for mutable log accumulation.
-data Logger w a
+data Logger a
   = LoggerPure a
-  | LoggerIO (IORef w -> IO a)
+  | LoggerIO (IORef MultipleErrors -> IO a)
 
 -- | Run a Logger computation given an existing IORef.
-runLogger :: Logger w a -> IORef w -> IO a
+runLogger :: Logger a -> IORef MultipleErrors -> IO a
 runLogger (LoggerPure a) _ = return a
 runLogger (LoggerIO f) r = f r
 
 -- | Run a Logger computation, starting with an empty log.
-runLogger' :: Monoid w => Logger w a -> IO (a, w)
+runLogger' :: Logger a -> IO (a, MultipleErrors)
 runLogger' l = do
   ref <- newIORef mempty
   a <- runLogger l ref
-  w <- readIORef ref
-  return (a, w)
+  (MultipleErrors list) <- readIORef ref
+  return (a, MultipleErrors $ reverse list)
 
 -- Functor
-instance Functor (Logger w) where
+instance Functor Logger where
   fmap f (LoggerPure a) = LoggerPure (f a)
   fmap f (LoggerIO m) = LoggerIO $ \r -> fmap f (m r)
 
 -- Applicative
-instance Monoid w => Applicative (Logger w) where
+instance Applicative Logger where
   pure = LoggerPure
   (<*>) = ap
 
 -- Monad
-instance Monoid w => Monad (Logger w) where
+instance Monad Logger where
   return = pure
   LoggerPure a >>= f = f a
   LoggerIO m >>= f = LoggerIO $ \r -> do
@@ -50,12 +51,15 @@ instance Monoid w => Monad (Logger w) where
     runLogger (f a) r
 
 -- MonadIO
-instance Monoid w => MonadIO (Logger w) where
+instance MonadIO Logger where
   liftIO = LoggerIO . const
 
 -- MonadWriter
-instance Monoid w => MonadWriter w (Logger w) where
-  tell w = LoggerIO $ \r -> atomicModifyIORef' r $ \w' -> (mappend w' w, ())
+instance MonadWriter MultipleErrors Logger where
+  tell w = LoggerIO $ \r ->
+    atomicModifyIORef' r $ \(MultipleErrors acc) ->
+      let MultipleErrors new = w
+       in (MultipleErrors (new ++ acc), ())
   listen m = LoggerIO $ \r -> do
     (a, w) <- runLogger' m
     atomicModifyIORef' r $ \w' -> (mappend w' w, (a, w))
@@ -64,12 +68,12 @@ instance Monoid w => MonadWriter w (Logger w) where
     atomicModifyIORef' r $ \w' -> (mappend w' (f w), a)
 
 -- MonadBase
-instance Monoid w => MonadBase IO (Logger w) where
+instance MonadBase IO Logger where
   liftBase = liftIO
 
 -- MonadBaseControl
-instance Monoid w => MonadBaseControl IO (Logger w) where
-  type StM (Logger w) a = a
+instance MonadBaseControl IO Logger where
+  type StM Logger a = a
   liftBaseWith f = LoggerIO $ \r -> liftBaseWith $ \runInBase ->
     f (\m -> runInBase (runLogger m r))
   restoreM = return
